bec84955f92d831d3dddad37bdafedc4c1b2b288
flolauck <flolauck@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2009-09-22 23:41:12 +0000

    Correction of last commit, now it works: 	* sessions.py uses module shelve which, depending on the OS and Python version, adds a '.db' to the session filename 	* this is taken care of now by catching the resulting error and calling the 'right' function
    Revision 3 is missing commit comments:
    	* mainly changes to make the code more stable regarding directories
    	* rather than using absolute filenames and directory names it uses the environment variables now
    	* only exception: parameters.conf must live in /etc/rosettaweb/
    	* alascan2.py uses parameters.conf for database related variables only
    
    
    
    
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@5 fc5780ad-6544-46e6-b60b-86c75d3988a5

--- backrub/frontend/session.py
+++ backrub/frontend/session.py
@@ -32,9 +32,9 @@ class Session(object):
       except OSError, e:
           errmsg =  """%s when trying to create the session directory. Create it as '%s'""" % (e.strerror, os.path.abspath(session_dir))
           raise OSError, errmsg
+    self.data = shelve.open(session_dir + '/sess_' + sid, writeback=True) # this ads an additional '.db' to the filename ... weird!
     try:
-      self.data = shelve.open(session_dir + '/sess_' + sid, writeback=True) # this ads an additional '.db' to the filename ... weird!
-      # os.chmod(session_dir + '/sess_' + sid , 0660)
+      os.chmod(session_dir + '/sess_' + sid , 0660)
     except OSError:
       # this is specific to the python version or the database used by shelve
       os.chmod(session_dir + '/sess_' + sid + '.db', 0660)
