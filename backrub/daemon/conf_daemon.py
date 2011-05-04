93732b9c96f0e28cfa46dc3278810b982f2d3b82
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2011-05-04 19:14:22 +0000

    Adding a constants file for the daemon.
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@276 fc5780ad-6544-46e6-b60b-86c75d3988a5
    
    Former-commit-id: f7888cbc02b14e4ee8c975e694dedf564643da8a

---
+++ backrub/daemon/conf_daemon.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.4
+# -*- coding: iso-8859-15 -*-
+
+########################################
+# Constants for the daemon and cluster code
+########################################
+
+_debugmode = False
+clusterRootDir = "/netapp/home/klabqb3backrub"
+netappRoot = "%s/temp"
+# todo: change to something sensible
+resultsRoot = "/home/oconchus/clustertest110428/rosettawebclustertest/backrub/daemon/cluster/output"
+inputDirectory = "/home/oconchus/clustertest110428/rosettawebclustertest/backrub/daemon/cluster/input/"
