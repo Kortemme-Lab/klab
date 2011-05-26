f907ff857a501c49acfc32dfd911de5888630020
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2011-05-26 22:02:58 +0000

    Debugging frontend.
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@304 fc5780ad-6544-46e6-b60b-86c75d3988a5
    
    Former-commit-id: 5cbbf332304e9732a3cab67a96798884505c7837

--- backrub/daemon/cluster/Graph.py
+++ backrub/daemon/cluster/Graph.py
@@ -10,8 +10,10 @@ Copyright (c) 2011 __UCSF__. All rights reserved.
 import sys
 sys.path.insert(0, "../")
 sys.path.insert(0, "../../common/")
+print(sys.path)
 import traceback
 from string import join
+import rosettahelper
 from rosettahelper import readFile, writeFile
 import ClusterTask  
 
