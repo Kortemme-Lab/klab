4ce58278c5de123a8ece4267ee3bbacaabc42c0a
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2011-06-16 23:31:19 +0000

    Updated cron job to check cluster daemon as well.
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@350 fc5780ad-6544-46e6-b60b-86c75d3988a5
    
    Former-commit-id: 880942caa12dacfad8f74ba63038961d4a5fae6e

--- backrub/cron/checkpid.sh
+++ backrub/cron/checkpid.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ ! -f /tmp/daemon-example.pid ] 
+if [ ! -f /tmp/rosettaweb-rosettadaemon.pid ] 
 then
-  echo "The backrub webserver daemon needs to be rebooted." | mail -s "Backrub failure" spiderbaby@gmail.com
-  echo "The backrub webserver daemon needs to be rebooted." | mail -s "Backrub failure" shane.oconnor@ucsf.edu
+  echo "The backrub webserver daemon needs to be rebooted." | mail -s "Backrub webserver daemon failure" spiderbaby@gmail.com
+  echo "The backrub webserver daemon needs to be rebooted." | mail -s "Backrub webserver daemon failure" shane.oconnor@ucsf.edu
 fi
 
-#echo "The backrub webserver daemon needs to be rebooted" | mail -s "Backrub failure" shane.oconnor@ucsf.edu
-#if [ ! -f /tmp/daemon-example.pid ] then
-# echo "The backrub webserver daemon needs to be rebooted" | mail -s "Backrub failure" spiderbaby@gmail.com
-#fi
+if [ ! -f /tmp/rosettaweb-clusterdaemon.pid ]
+then
+  echo "The backrub cluster daemon needs to be rebooted" | mail -s "Backrub cluster daemon failure" shane.oconnor@ucsf.edu
+  echo "The backrub cluster daemon needs to be rebooted" | mail -s "Backrub cluster daemon failure" spiderbaby@gmail.com
+fi
