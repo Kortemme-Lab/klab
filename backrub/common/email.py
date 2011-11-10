d9e0d40797164467411820efe62a51b66475597e
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2011-11-10 22:21:56 +0000

    Adding common code for sending emails.
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@497 fc5780ad-6544-46e6-b60b-86c75d3988a5
    
    Former-commit-id: 6fe736278dc4b7bc7df3f9815cf3fcfc96b7c585

---
+++ backrub/common/email.py
@@ -0,0 +1,78 @@
+from string import join 
+import email.Message
+import smtplib
+from email.MIMEMultipart import MIMEMultipart 
+from email.MIMEText import MIMEText
+
+default_email = "shane.oconnor@ucsf.edu"
+
+def sendEmail(subject, sender, recipients, plaintext, htmltext = None, cc = None, debug = False, useMIMEMultipart = True):
+	if recipients:
+		#if type(recipients) == type([]):
+		#	recipients = join(recipients, ";")
+		if type(recipients) == type(""):
+			recipients = [recipients]
+		elif type(recipients) != type([]):
+			raise Exception()
+		if cc:
+			if type(cc) == type(""):
+				recipients.append(cc)
+			elif type(cc) == type([]):
+				recipients.extend(cc)
+			else:
+				raise Exception()
+			
+		if useMIMEMultipart:
+			msg = MIMEMultipart('alternative')
+		else:
+			msg = email.Message.Message()
+			
+		msg['Subject'] = subject
+		msg['From'] = sender
+		msg['To'] = recipients
+		msg['Reply-To'] = default_email
+		if useMIMEMultipart:
+			part1 = MIMEText(plaintext, 'plain')
+			part2 = MIMEText(htmltext, 'html')
+			msg.attach(part1)
+			msg.attach(part2)
+		else:
+			msg.set_type("text/plain")
+			msg.set_payload(plaintext)
+		
+		if debug:
+			print(msg)
+		else:
+			s = smtplib.SMTP()
+			s.connect()
+			s.sendmail(msg['From'], recipients, msg.as_string())
+			s.close()
+		return True
+	return False
+
+def sendEmail2(subject, sender, recipients, body, cc = None, debug = False):
+	if recipients:
+		if type(recipients) == type(""):
+			recipients = [recipients]#
+			#join(recipients, ";")
+		msg = email.Message.Message()
+		msg['Subject'] = subject
+		msg['From'] = sender
+		msg['To'] = recipients
+		msg['Reply-To'] = default_email
+		
+		msg.set_type("text/plain")
+		msg.set_payload(body)
+		
+		if debug:
+			print(msg)
+		else:
+			s = smtplib.SMTP()
+			s.connect()
+			if cc:
+				s.sendmail(msg['From'], [msg['To'], cc], msg.as_string())
+			else:
+				s.sendmail(msg['From'], [msg['To']], msg.as_string())
+			s.close()
+		return True
+	return False
