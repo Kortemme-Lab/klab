1de66daf1a3d43bc0d3d2c8aaac3c9ee3fc1e32d
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2011-12-05 22:11:06 +0000

    This version is similar to the version in the publiccode project at this revision. Changed Script to an abstract class and added a concrete derived class, SimpleScript. Moved some code into SimpleScript. Added a warnings section to the email text. Added more comments.
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@533 fc5780ad-6544-46e6-b60b-86c75d3988a5
    
    Former-commit-id: 131b0374b15c85e4354a4f40f2faaefbf0558190

--- backrub/common/retrospect.py
+++ backrub/common/retrospect.py
@@ -12,17 +12,41 @@ RETROSPECT_UNHANDLED_ERROR = 8
 RETROSPECT_WARNING = 16
 RETROSPECT_FAIL = 32
 
+# The amount of extra leeway in days to allow before we consider a job to have stopped running
+# This value is used by default for the LogReader functions. 
+TYPICAL_LAPSE = 1.5
+
+# If you call these functions from multiple scripts, you can use this value to ensure all scripts read the same
+# amount of log
+DEFAULT_LOG_SIZE = 25 * 65536 # Read 25x64KB from the end of the log. Change this value to suit
+
+SECONDS_IN_A_DAY = 60 * 60 * 24
+
+
 class Script(object):
+	
+	def __init__(self):
+		raise Exception("This is an abstract class. Define a concrete derived class.")
+	
+	def check(self, dateOfLastSuccess):
+		raise Exception("This is an abstract class. Define a concrete derived class.")
+
+class SimpleScript(Script):
 	
 	def __init__(self, allowedLapse):
 		self.allowedLapse = allowedLapse
 	
-	def check(self, lapse):
+	def check(self, dateOfLastSuccess, extralapse):
+		lapse = self.getDaysSinceLastSuccess(dateOfLastSuccess) - extralapse
 		if lapse > self.allowedLapse:
 			return False
 		else:
 			return True
 	
+	def getDaysSinceLastSuccess(self, dateOfLastSuccess):
+		td = (datetime.datetime.today() - dateOfLastSuccess)
+		return td.days + (float(td.seconds) / float(SECONDS_IN_A_DAY))
+			
 class ExpectedScripts(object):
 	
 	def __init__(self):
@@ -39,19 +63,20 @@ class ExpectedScripts(object):
 		for k in self.scripts.keys():
 			result = self.check(k) and result
 	
-	def check(self, scriptname, criteria):
-		return self.scripts[scriptname].check(criteria)
-	
+	def check(self, scriptname, dateOfLastSuccess, criteria):
+		return self.scripts[scriptname].check(dateOfLastSuccess, criteria)
+				
 	def SymmetricDifference(self, scriptnames):
 		'''Takes in a set, list, or tuple scriptnames and returns the symmetric difference (as a list)
 		of scriptnames and the stored names.'''
 		scriptnames = set(scriptnames)
 		myscripts = set(self.scripts.keys())
 		return list(scriptnames.difference(myscripts).union(myscripts.difference(scriptnames)))
-		
+
+
 class LogReader(object):
 	
-	def __init__(self, logfile, maxchars, expectedScripts):
+	def __init__(self, logfile, expectedScripts, maxchars = DEFAULT_LOG_SIZE):
 		'''This function reads up to maxchars of the logfile. 
 		   
 		   It creates a dict for the object called log which maps job dates to dicts with the keys "script" (jobname),
@@ -62,7 +87,8 @@ class LogReader(object):
 		   from the most recent run made up from the flags above), "lastRun" (the date of the most recent run), and
 		   "lastSuccess" (the date of the most recent success).
 		   
-		   Finally, we check whether all scripts in expectedScripts   
+		   Finally, we check whether all scripts in expectedScripts are found and report the missing scripts as
+		   failures in scriptsRun. 
 		   '''
 		
 		self.log = None
@@ -149,7 +175,7 @@ class LogReader(object):
 						jobmatch = engineStartTime.match(record[1])
 			
 			# NOTE: If we could not match the record to one of the above types, it is not stored in the log.
-			# Instead, we just print out the record to the terminal for debugging.
+			# Instead, we just print out the record for debugging.
 			if type and jobmatch:
 				record[-1] = record[-1].strip()
 	
@@ -255,6 +281,7 @@ class LogReader(object):
 					scriptsRun[rscript] = {"status" : status, "lastRun" : dt, "lastSuccess" : lastSuccess} 
 				
 			else:
+				# Not pretty but it will be displayed pretty obviously
 				print(join(record,"<br>") + "<br><br>")
 		
 		# Check against expected scripts
@@ -267,27 +294,22 @@ class LogReader(object):
 				del scriptsRun[t]
 		for ms in missingScripts:
 			scriptsRun[ms] = {"status" : RETROSPECT_FAIL, "lastRun" : None, "lastSuccess" : None} 
-				
+			
 		self.log = log
 		self.scriptsRun = scriptsRun
-
-	def getLog(self):
-		return self.log
 	
-	def getEarliestEntry(self):
-		return self.earliestEntry
-	
-	def getFailedJobIDs(self):
-		'''Returns a list of time stamps which identify failed jobs in the scriptsRun table.''' 
+	def getFailedJobIDs(self, extraLapse = TYPICAL_LAPSE):
+		'''Returns a list of which identify failed jobs in the scriptsRun table.
+			If a time stamp for a job can be found, we return this. The time stamp can be used to index the log.
+			If no time stamp was found, return the name of the script instead.
+		''' 
 		scriptsRun = self.scriptsRun
 		
 		failedJobTimestamps = []
 		nodata = []
 		for name, details in sorted(scriptsRun.iteritems()):
 			if details["lastSuccess"] and expectedScripts.get(name):
-				td = (datetime.datetime.today() - details["lastSuccess"])
-				days = td.days + (float(td.seconds) / float(60 * 60 * 24))
-				if not expectedScripts.check(name, days - 1.5): # Allow two days of grace period before indicating failure
+				if not expectedScripts.check(name, details["lastSuccess"], extraLapse): 
 					if details["lastRun"]:
 						failedJobTimestamps.append(details["lastRun"])
 					else:
@@ -307,22 +329,28 @@ class LogReader(object):
 		
 		return failedJobTimestamps, nodata
 	
-	def generateSummary(self):
+	def generateSummary(self, extraLapse = TYPICAL_LAPSE):
+		'''Generates a summary of the status of the expected scripts broken based on the log.
+			This summary (a list of strings) is returned as well as a list with the dates (which
+			can be used to index the log)of the most recent attempts at the failed jobs. 
+		'''
 		scriptsRun = self.scriptsRun
 		
 		body = []
 		
 		numberOfFailed = 0
+		numberWithWarnings = 0
 		failedList = []
 		successList = []
+		warningsList = []
+		
 		for name, details in sorted(scriptsRun.iteritems()):
 			status = None
 			daysSinceSuccess = None
 			
 			if details["lastSuccess"] and expectedScripts.get(name):
-				td = (datetime.datetime.today() - details["lastSuccess"])
-				daysSinceSuccess = td.days + (float(td.seconds) / float(60 * 60 * 24))
-				if not expectedScripts.check(name, daysSinceSuccess - 1.5): # Allow two days of grace period before indicating failure
+				daysSinceSuccess = expectedScripts.get(name).getDaysSinceLastSuccess(details["lastSuccess"])
+				if not expectedScripts.check(name, details["lastSuccess"], extraLapse): 
 					status = "FAILED"
 			else:
 				status = "FAILED"
@@ -335,21 +363,36 @@ class LogReader(object):
 				elif status != "FAILED":
 					status = "OK"
 			
+			if details["lastSuccess"] and daysSinceSuccess:
+				lastSuccessDetails = "Last successful run on %s (%0.1f days ago)" % (details["lastSuccess"], daysSinceSuccess)
+			else:
+				lastSuccessDetails = "No recent successful run."
 			
+			if details["lastRun"]:
+				lastRunDetails = "Last run on %s (%s)" % (details["lastRun"], status)
+			else:
+				lastRunDetails = "No recent run (%s)" % status
+				 
 			if status == "FAILED":
 				numberOfFailed += 1
-				if details["lastSuccess"]:
-					failedList.append("%s: Last run %s (%s), last successful run %s (%0.1f days ago)" % (name, details["lastRun"], status, details["lastSuccess"], daysSinceSuccess))
-				else:
-					failedList.append("%s: Last run %s (%s), no recent successful run." % (name, details["lastRun"], status))
+				failedList.append("%s: %s. %s" % (name, lastRunDetails, lastSuccessDetails))
+			elif status == "WARNINGS":
+				numberWithWarnings += 1
+				warningsList.append("%s: %s. %s" % (name, lastRunDetails, lastSuccessDetails))
 			else:
-				successList.append("%s: Last run %s (%s), last successful run %s (%0.1f days ago)" % (name, details["lastRun"], status, details["lastSuccess"], daysSinceSuccess))
+				successList.append("%s: %s. %s" % (name, lastRunDetails, lastSuccessDetails))
 		
 		body = []
 		if failedList:
-			body.append("FAILED JOBS")
-			body.append("***********")
+			body.append("FAILED JOBS (%d)" % numberOfFailed)
+			body.append("****************")
 			for j in failedList:
+				body.append(j)
+			body.append("\n")
+		if warningsList:
+			body.append("JOBS WITH WARNINGS (%d)" % numberWithWarnings)
+			body.append("***********************")
+			for j in warningsList:
 				body.append(j)
 			body.append("\n")
 		if successList:
@@ -359,29 +402,34 @@ class LogReader(object):
 				body.append(j)
 		
 		return body, failedList
-		
 	
-	def generateSummaryHTMLTable(self):
+	def createAnchorID(self, scriptname, date):
+		'''This creates a string to be used in an anchor (<a>) HTML tag.'''
+		return ("%s%s" % (scriptname, str(date))).replace(" ","")
+	
+	def generateSummaryHTMLTable(self, extraLapse = TYPICAL_LAPSE):
+		'''Generates a summary in HTML of the status of the expected scripts broken based on the log.
+			This summary is returned as a list of strings. 
+		'''
 		scriptsRun = self.scriptsRun
 		
 		html = []
 		html.append("<table style='text-align:center;border:1px solid black;margin-left: auto;margin-right: auto;'>\n") # Start summary table
 		html.append('	<tr><td colspan="4" style="text-align:center"></td></tr>\n')
 		html.append('	<tr style="font-weight:bold;background-color:#cccccc;text-align:center"><td>Script</td><td>Last status</td><td>Last run</td><td>Last success</td></tr>\n')
+		
+		# Alternate shades between rows
 		tablestyle = ['background-color:#33dd33;', 'background-color:#33ff33;']
 		warningstyle = ['background-color:#EA8737;', 'background-color:#f5b767;']
 		failstyle = ['background-color:#dd3333;', 'background-color:#ff3333;']
-		count = 0
 		
+		count = 0
 		for name, details in sorted(scriptsRun.iteritems()):
 			status = None
 			
 			rowstyle = tablestyle[count % 2]
 			if details["lastSuccess"] and expectedScripts.get(name):
-				td = (datetime.datetime.today() - details["lastSuccess"])
-				days = td.days + (float(td.seconds) / float(60 * 60 * 24))
-				if not expectedScripts.check(name, days - 1.5): # Allow two days of grace period before indicating failure
-					rowstyle = failstyle[count % 2]		 
+				if not expectedScripts.check(name, details["lastSuccess"], extraLapse): 
 					status = "STOPPED"
 			else:
 				rowstyle = failstyle[count % 2]
@@ -396,34 +444,66 @@ class LogReader(object):
 			elif status != "FAIL" and status != "STOPPED":
 				laststatusstyle = tablestyle[count % 2]
 				status = "OK"
-				
+			
+			# Start a row	
 			html.append('<tr style="text-align:left;%s">\n' % rowstyle)
-			html.append('\t<td>%s</td>\n' % name)
+			
+			# Script name field
+			if status == "STOPPED":
+				html.append('\t<td style="%s">%s</td>\n' % (failstyle[count % 2], name))
+			else:
+				html.append('\t<td>%s</td>\n' % name)
+				
+			# Last status field
 			if details["lastRun"]:
-				html.append('\t<td style="%s"><a href="#%s">%s</a></td>\n' % (laststatusstyle, ("%s%s" % (name, str(details["lastRun"]))).replace(" ",""), status))
+				if status == "STOPPED":
+					html.append('\t<td style="%s"><a href="#%s">%s</a></td>\n' % (failstyle[count % 2], self.createAnchorID(name, details["lastRun"]), status))
+				else:
+					html.append('\t<td style="%s"><a href="#%s">%s</a></td>\n' % (laststatusstyle, self.createAnchorID(name, details["lastRun"]), status))
 			else:
 				html.append('\t<td style="%s">%s</td>\n' % (laststatusstyle, status))
+			
+			# Last run field
 			if details["lastRun"]:
-				html.append('\t<td style="%s"><a href="#%s">%s</a></td>\n' % (laststatusstyle, ("%s%s" % (name, str(details["lastRun"]))).replace(" ",""), details["lastRun"]))
+				html.append('\t<td style="%s"><a href="#%s">%s</a></td>\n' % (laststatusstyle, self.createAnchorID(name, details["lastRun"]), details["lastRun"]))
 			else:
-				html.append('\t<td style="%s">none since %s</td>\n' % (laststatusstyle, self.earliestEntry))
+				html.append('\t<td style="%s">none found</td>\n' % laststatusstyle)
+			
+			# Last success field
 			if details["lastSuccess"]:
-				html.append('\t<td><a href="#%s">%s</a></td>\n' % (("%s%s" % (name, str(details["lastSuccess"]))).replace(" ",""), details["lastSuccess"]))
+				html.append('\t<td><a href="#%s">%s</a></td>\n' % (self.createAnchorID(name, details["lastSuccess"]), details["lastSuccess"]))
 			else:
-				html.append('\t<td>none since %s</td>\n' % self.earliestEntry)
+				html.append('\t<td>none found</td>\n')
 			html.append('</tr>\n')
 			count += 1
 		html.append("</table>")
 		return html
 
+	# ACCESSORS
+	def getLog(self):
+		return self.log
+	
+	def getEarliestEntry(self):
+		return self.earliestEntry
+# End of class
+
+# Add script names (e.g. "MyPC") expected to run daily here 
 expectedScripts = ExpectedScripts()
+for s in []:
+	expectedScripts.add(s, SimpleScript(1))
+# Add script names (e.g. "EveryoneElsesPC") expected to run weekly here 
+for s in []:
+	expectedScripts.add(s, SimpleScript(7))
+
+	
+# Private
 for s in ["kortemme-cd", "kortemme-fplc", "kortemme-hplc", "kortemme-spec",
 		"kortemmelab-admin", "kortemmelab-data", "kortemmelab-home",
 		"AmelieStein", "Bobal", "Kortemme", "Lab Admin",
 		"Epona", "Ganon", "Web Server", "Workstations"]:
-	expectedScripts.add(s, Script(1))
+	expectedScripts.add(s, SimpleScript(1))
 for s in ["kortemme-cd (offsite)", "kortemme-fplc (offsite)", "kortemme-hplc (offsite)", "kortemme-spec (offsite)",
 		"kortemmelab-alumni-data", "kortemmelab-alumni-home",
-		"AmelieStein", "Bobal (offsite)", "Kortemme (offsite)", "Lab Admin (offsite)",
+		"Amelie (offsite)", "Bobal (offsite)", "Kortemme (offsite)", "Lab Admin (offsite)",
 		"Epona (offsite)", "Ganon", "Web Server (offsite)", "Workstations (offsite)"]:
-	expectedScripts.add(s, Script(7))
+	expectedScripts.add(s, SimpleScript(7))
