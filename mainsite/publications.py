d7cfac5b6f53b1a17803a1747cc6be5ea5228bed
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2011-12-14 00:31:30 +0000

    The publications list now has all the records with normalized names. The script parses the list and checks for bad / duplicate / non-normalized author names.
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@543 fc5780ad-6544-46e6-b60b-86c75d3988a5
    
    Former-commit-id: 06e7c78328b64789ab030a3f460d32ac1c6135ce

--- mainsite/publications.py
+++ mainsite/publications.py
@@ -5,6 +5,7 @@ sys.path.insert(-1, "../backrub/common")
 import colortext
 
 lsttype = type([])
+strtype = type("")
 
 taglist = [
 'TY',#  - Type of reference (must be the first tag)
@@ -63,8 +64,9 @@ taglist = [
 'UR',#  - URL
 'VL',#  - Volume
 'Y2',#  - Access Date
+'BT',#  - Title Primary.
 'ER',#  - End of Reference (must be the last tag)
-#
+ #
 'VO', # - Volume?
 'URL', # Tag Shane added
 'L3', # Link?
@@ -78,7 +80,7 @@ header  = '''
               <td style="width: 486px;">
                 <img src="img/publications.jpg" alt="" style="border: 0px solid ; width: 271px; height: 35px;">
                 <br>
-                <div style="text-align:right"><a class="style14 publist" href="pubs.ris">bibliography (RIS)</a></div>
+                <div style="text-align:right"><a class="style14 publist" href="publications.ris">bibliography (RIS)&nbsp;</a></div>
 '''
 
 sectionheader = '''
@@ -134,77 +136,135 @@ def parsePublist(saveRis = True):
 	contents = F.read()
 	F.close()
 	
-	publist = []
-	risfile = []
-	#contents=contents.split("[[")
-	
-	#sections = re.split(r'\[\[(.*?)]](.*?)', contents)
-	sections = re.split('(\[\[.*?]].*)', contents)
-	if not sections[0]:
-		sections = sections[1:]
-	assert(len(sections) % 2 == 0)
-	sections = [(sections[i], sections[i + 1]) for i in range(0, len(sections), 2)]
-	
-	count = 0
-	print("")
-	for section in sections:
-		sectiontitle = section[0].replace("[[","")
-		sectiontitle = sectiontitle.replace("]]","")
-		
-		risfile.append(section[1].strip())
-		pubs = section[1].split("ER  -")
-		pubsBySection = []
-		for pub in pubs:
-			pub = pub.strip()
-			errors = []
-			if pub:
-				d = parsePub(pub)
-				authors = join(d.get("AU",[]), ", ")
-				title = d.get("TI")
-				journal = d.get("JO")
-				volume = d.get("VL")
-				issue = d.get("IS")
-				startpage = d.get("SP")
-				endpage = d.get("EP")
-				if type(journal) == lsttype:
-					journal = journal[0]
-				if volume:
-					entry = volume
-					if issue:
-						entry += "(%s)" % issue
-					else:
-						errors.append("No issue found.")
-					if startpage and endpage:
-						entry += ":%s-%s" % (startpage, endpage)
-					else:
-						errors.append("No start or endpage found.")
-					#entry = "%s(%s):%s-%s" % (d.get("VL"), d.get("IS"), d.get("SP"), d.get("EP"))
-				else:
-					if startpage and endpage and startpage.isdigit() and endpage.isdigit():
-						entry += "%s-%s" % (startpage, endpage)
-					else:
-						errors.append("No start or endpage found.")
-				doi = d.get("M3")
-				if doi and doi.startswith("doi: "):
-					doi = doi.split(":")[1].strip()
-				pubsBySection.append({"authors" : authors, "title" : title, "journal" : journal, "entry" : entry, "doi" : doi, "URL" : d.get("URL")})
-				if errors:
-					colortext.warning(title)
-					for e in errors:
-						colortext.error("\t%s" % e)
-				#%s. %s. %s. %s. %s" % (authors, title, journal, entry, doi))
-		publist.append((sectiontitle, pubsBySection))
-		count += 1
-		if count == 3:#todo
+	# Create public RIS file
+	risfile = contents.split("\n")
+	for i in range(len(risfile)):
+		if risfile[i].startswith("ER"):
+			risfile = risfile[i+1:]
 			break
-	
+	risfile = join([line for line in risfile if line[0:2] != "C8"], "\n")
 	if saveRis:
 		F = open("publications.ris", "w")
-		F.write(join(risfile, "\n"))
+		F.write(risfile)
 		F.write("\n")
 		F.close()
-	else:
-		print(join(risfile, "\n"))
+	
+	publist = []
+	
+	count = 0
+		
+	contents.split("\n")
+	
+	authornames = {}
+	sectionTitles = []
+	pubsBySection = {}
+	pubs = contents.split("ER  -")[1:] # Ignore the comments before the first entry
+	for pub in pubs:
+		pub = pub.strip()
+		errors = []
+		if pub:
+			d = parsePub(pub)
+			
+			if not d:
+				break
+			
+			# Get the section title from the custom field and populate the ordered lists
+			if not d.get("C8") or type(d["C8"]) == lsttype:
+				errors.append("There is a missing or duplicate value for C8 which we reserve for section titles.")
+				break
+			sectiontitle = d.get("C8")
+			if sectiontitle not in sectionTitles:
+				sectionTitles.append(sectiontitle)
+				pubsBySection[sectiontitle] = []
+				
+			title = d.get("TI")
+			for author in d.get("AU",[]):
+				authornames[author] = True
+			authors = join(d.get("AU",[]), ", ")
+			journal = d.get("JO")
+			volume = d.get("VL")
+			issue = d.get("IS")
+			startpage = d.get("SP")
+			endpage = d.get("EP")
+			if type(journal) == lsttype:
+				journal = journal[0]
+			if volume:
+				entry = volume
+				if issue:
+					entry += "(%s)" % issue
+				else:
+					errors.append("No issue found.")
+				if startpage and endpage:
+					entry += ":%s-%s" % (startpage, endpage)
+				else:
+					errors.append("No start or endpage found.")
+				#entry = "%s(%s):%s-%s" % (d.get("VL"), d.get("IS"), d.get("SP"), d.get("EP"))
+			else:
+				if startpage and endpage and startpage.isdigit() and endpage.isdigit():
+					entry += "%s-%s" % (startpage, endpage)
+				else:
+					errors.append("No start or endpage found.")
+			for k, v in d.iteritems():
+				if type(v) == strtype and v.startswith("doi:"):
+					doi = v[4:].strip()
+			#doi = d.get("M3")
+			#if doi and doi.startswith("doi: "):
+			#	doi = doi.split(":")[1].strip()
+			if not(doi or d.get("URL")):
+				errors.append("No DOI or URL available.")
+			pubsBySection[sectiontitle].append({"authors" : authors, "title" : title, "journal" : journal, "entry" : entry, "doi" : doi, "URL" : d.get("URL")})
+			if errors:
+				colortext.warning("%s (%s)" % (title, sectiontitle))
+				for e in errors:
+					colortext.error("\t%s" % e)
+				#%s. %s. %s. %s. %s" % (authors, title, journal, entry, doi))
+	colortext.message('''
+Authors - change names which have duplicate forms to the same long format (full name and surname).
+Some potential duplicates are highlighted below but the highlighting neither comprehensive or failsafe.
+Yellow potentially indicates a duplicate name, cyan potentially indicates a missing period or first name.''')
+	lastSurname = None
+	lastInitial = None
+	for author in sorted(authornames.keys()):
+		names = [n for n in re.split(r'[, ]', author) if n]
+		surname = names[0]
+		initial = None
+		if len(names) > 1:
+			initial = names[1][0]
+		
+		# Catch missing periods in abbreviated names
+		maybeMissingPeriodOrFirstName = False
+		numberOfFullNames = 0
+		for n in names:
+			firstPeriod = n.find(".")
+			if firstPeriod != -1 and firstPeriod != len(n) - 1:
+				maybeMissingPeriodOrFirstName = True
+			if len(n) == 1:
+				maybeMissingPeriodOrFirstName = True
+			elif n[1] != ".":
+				numberOfFullNames += 1
+		if numberOfFullNames < 2:
+			maybeMissingPeriodOrFirstName = True
+			
+		if lastSurname and surname == lastSurname:
+			if not(initial):
+				colortext.warning(author)
+			elif initial == lastInitial:
+				colortext.warning(author)
+			else:
+				if maybeMissingPeriodOrFirstName:
+					colortext.printf(author, color = "cyan")
+				else:
+					print(author)
+		elif maybeMissingPeriodOrFirstName:
+			colortext.printf(author, color = "cyan")
+		else:
+			print(author)
+		lastSurname = surname
+		lastInitial = initial
+		
+	for sectiontitle in sectionTitles:
+		publist.append((sectiontitle, pubsBySection[sectiontitle]))
+		
 	return publist
 
 def getHTML(page):
@@ -228,4 +288,6 @@ def getHTML(page):
 	html.append(footer)
 	return join(html)
 
-#SAT-based protein design is missing in PubMed
+#Missing in PubMed:
+# Morozov, AV, Kortemme, T, & Baker, D. Evaluation of models of electrostatic interactions in proteins. J. Phys. Chem. B 107, 2075-2090, 2003.
+# Ollikainen N, Sentovich E, Coelho C, Kuehlmann A, Kortemme, T. (2009). SAT-based protein design. Proceedings of the 2009 IEEE/ACM International Conference on Computer-Aided Design (ICCAD 2009): 128-35.
