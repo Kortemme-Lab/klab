4c890054404b732624b1a569274436618849b5b0
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2012-02-13 20:21:22 +0000

    Moved ClusterBatchJob into tasks.py (soon to be rerererenamed) and deleted that file.
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@592 fc5780ad-6544-46e6-b60b-86c75d3988a5
    
    Former-commit-id: 1c9fcadcbcddd281684acbf7f21d0df41e3286fe

--- backrub/daemon/ddG/tasks.py
+++ backrub/daemon/ddG/tasks.py
@@ -12,20 +12,18 @@ import re
 import shutil
 import glob
 from string import join, split
-import traceback
 import fnmatch
-import rosettahelper
 from conf_daemon import *
-from ClusterScheduler import TaskScheduler
-from ddGClusterScheduler import ClusterBatchJob
+from ClusterScheduler import checkGraphReachability, traverseGraph, TaskSchedulerException, SchedulerTaskAddedAfterStartException, BadSchedulerException, TaskCompletionException, SchedulerDeadlockException, SchedulerStartException
+from ClusterScheduler import TaskScheduler, RosettaClusterJob 
 from RosettaTasks import PostProcessingException
 import ddgproject
 ddgfields = ddgproject.FieldNames()
 from conf_daemon import CLUSTER_maxhoursforjob, CLUSTER_maxminsforjob, clusterRootDir
 import SimpleProfiler
-from rosettahelper import make755Directory, writeFile
 from ClusterTask import INITIAL_TASK, INACTIVE_TASK, QUEUED_TASK, ACTIVE_TASK, RETIRED_TASK, COMPLETED_TASK, FAILED_TASK, status
 from ClusterTask import ClusterScript, ClusterTask
+from rosettahelper import make755Directory, makeTemp755Directory, writeFile, permissions755, permissions775, normalize_for_bash
 
 # Generic classes
 
@@ -201,7 +199,7 @@ class GenericDDGTask(ClusterTask):
 		commandLines = self.taskparameters["Command"]
 		for jobID, inputs in sorted(self.inputs.iteritems()):
 			for input in inputs:
-				commandLines = commandLines.replace("%(" + input + ")s", "$" + rosettahelper.normalize_for_bash(input) + "var")
+				commandLines = commandLines.replace("%(" + input + ")s", "$" + normalize_for_bash(input) + "var")
 			break
 		commandLines = commandLines.split("\n")
 		
@@ -213,8 +211,8 @@ class GenericDDGTask(ClusterTask):
 					errstr = "The input parameter %s has not been specified for job %d." % (k, jobID)
 					self._status(errstr)
 					raise Exception(errstr)
-				job_inputs[rosettahelper.normalize_for_bash(k)] = v
-				norm_key = rosettahelper.normalize_for_bash(k)
+				job_inputs[normalize_for_bash(k)] = v
+				norm_key = normalize_for_bash(k)
 				normalized_inputs[norm_key] = normalized_inputs.get(norm_key, {})
 				normalized_inputs[norm_key][jobID] = v
 			#normalized_inputs[jobID] = job_inputs
@@ -346,6 +344,91 @@ class GenericDDGTask(ClusterTask):
 				for file in glob.glob(self._workingdir_file_path(p)):
 					shutil.copy(file, tdir)	
 		
+class ClusterBatchJob(RosettaClusterJob):
+
+	def __init__(self, sgec, parameters, tempdir, targetroot, dldir, testonly = False):
+		self.jobIDs = sorted(parameters["jobs"].keys()) # The fact that this is a sorted list is important
+		self.jobID = "%s-%s" % (parameters["cryptID"], parameters["ID"])
+		self.profiler = SimpleProfiler.SimpleProfiler("%s-%s" % (self.suffix, parameters["ID"]))
+		self.parameters = parameters
+		self.sgec = sgec
+		self.debug = True
+		self.tempdir = tempdir
+		self.targetroot = targetroot
+		self.testonly = testonly
+		self.dldir = os.path.join(dldir, parameters["cryptID"])
+		if not testonly:
+			self._make_workingdir()
+			self._make_targetdir()
+		else:
+			self.workingdir = "/home/oconchus/clustertest110428/rosettawebclustertest/backrub/temp/test" #todo
+			self.targetdirectory = "/home/oconchus/clustertest110428/rosettawebclustertest/backrub/temp/test"
+		self.profiler.PROFILE_START("Initialization")
+		self._initialize()
+		self.profiler.PROFILE_STOP("Initialization")
+		self.failed = False
+		self.error = None
+		self.resultFilemasks = []
+		self._defineOutputFiles()
+
+	def moveFilesTo(self, permissions = permissions755):
+		destpath = self.dldir
+		
+		self._status("Moving files to %s" % destpath)
+
+		if not os.path.exists(destpath):
+			make755Directory(destpath)
+			
+		timing_profile = self._targetdir_file_path("timing_profile.txt")
+		if os.path.exists(timing_profile):
+			shutil.move(timing_profile, destpath)
+
+		for jobID in self.jobIDs:
+			destjobpath = os.path.join(destpath, str(jobID))
+			targetjobpath = os.path.join(self.targetdirectory, str(jobID))
+			if os.path.exists(destjobpath):
+				shutil.rmtree(destjobpath)
+			make755Directory(destjobpath)
+			if self.resultFilemasks:
+				for mask in self.resultFilemasks:
+					fromSubdirectory = os.path.join(targetjobpath, mask[0])
+					toSubdirectory = os.path.join(destjobpath, mask[0])
+					self._status("Moving files from %s to %s using mask '%s'.\n" % (fromSubdirectory, toSubdirectory, mask[1]))
+					if not os.path.exists(toSubdirectory):
+						make755Directory(toSubdirectory)
+					for file in os.listdir(fromSubdirectory):
+						self._status("File: %s" % file)
+						if fnmatch.fnmatch(file, mask[1]):
+							try:
+								print(os.path.join(fromSubdirectory, file), toSubdirectory)
+								shutil.move(os.path.join(fromSubdirectory, file), toSubdirectory)
+								if not os.path.exists(os.path.join(toSubdirectory, file)):
+									self._status("Error moving.")
+								else:
+									self._status("Moved.")
+							except Exception, e:
+								self._status("Exception moving %s to %s: %s" % (os.path.join(fromSubdirectory, file), toSubdirectory, str(e)))
+			else:
+				if os.path.exists(destjobpath):
+					shutil.rmtree(destjobpath)
+				shutil.move(targetjobpath, destjobpath)
+
+		os.chmod( destpath, permissions )
+		return destpath
+
+	def _make_taskdir(self, dirname, files = []):
+		""" Make a subdirectory dirname in the working directory and copy all files into it.
+			Filenames should be relative to the working directory."""
+		if not self.testonly:
+			for jobID in self.jobIDs: 
+				taskdir = os.path.join(self.workingdir, str(jobID), dirname)
+				if not os.path.isdir(taskdir):
+					make755Directory(taskdir)
+				if not os.path.isdir(taskdir):
+					raise os.error
+				for file in files:
+					shutil.copyfile("%s/%s" % (self.workingdir, file), "%s/%s" % (taskdir, file))
+		return self.workingdir
 
 class GenericDDGJob(ClusterBatchJob):
 
