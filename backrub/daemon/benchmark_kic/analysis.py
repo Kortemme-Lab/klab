3406fd65e5b2e68bae04e1447c2025c397e1577c
oconchus <oconchus@fc5780ad-6544-46e6-b60b-86c75d3988a5>
2012-06-21 21:57:31 +0000

    Committing some broken code prior to a reshuffling of files.
    git-svn-id: file:///kortemmelab/shared/commonSVN/rosettaweb@737 fc5780ad-6544-46e6-b60b-86c75d3988a5
    
    Former-commit-id: 3ac6af68ab7a2f6dbbbaf71f0ca69f4ad1016976

--- backrub/daemon/benchmark_kic/analysis.py
+++ backrub/daemon/benchmark_kic/analysis.py
@@ -1,217 +1,75 @@
 #!/usr/bin/python
-#This file was developed and written by Roland A. Pache, Ph.D., Copyright (C) 2011, 2012.
+#This file was developed and written by Shane O'Connor based on code from Roland A. Pache, Ph.D., Copyright (C) 2011, 2012.
 #Analyses the KIC scientific benchmark and creates a pdf report of the results.
 
-#import libraries
-import re
 import os
 import time
 import sys
 import math
+import shutil
 import statsfns
 import rosettahelper
+from string import join
 
-def getEnergyStats(models):
-	energies=[]
-	for model in models:
-		if model.id!=None:
-			energies.append(model.total_energy)
-	return (min(energies),statsfns.median(energies),max(energies))
-
-def getRmsdStats(models):
-	rmsds=[]
-	for model in models:
-		if model.id!=None:
-			rmsds.append(model.loop_rms)
-	return (min(rmsds),statsfns.median(rmsds),max(rmsds))
-
-def scatterplot(outdir, sorted_models, best_model, outfile_name, top_X):
-	outfile = open(outfile_name, 'w')
-	outfile.write('#Model\tLoop_rmsd\tTotal_energy\n')
-	energies = []
-	for model in sorted_models:
-		outfile.write('%s\n' % str(model))
-		energies.append(model.total_energy)
-	#-
-	outfile.write('\n\n')
-	for i in range(min(len(sorted_models), 5)):
-		model = sorted_models[i]
-		outfile.write('%s\n' % str(model))
+class BenchmarkReport(object):
+	def __init__(self, outdir, reportsettings, quiet = False, html = False):
+		self.outdir = rosettahelper.makeTemp755Directory(outdir)
+		self.reportsettings = reportsettings
+		self.quiet = quiet
+		self.html = html
+		self.benchmarkRunSettings = []
+		self.PDFReport = None
+			
+	def __del__(self):
+		pass # shutil.rmtree(self.outdir)
+	
+	def addBenchmark(self, runID, description, flatfile, rosettaversion, rosettadbversion, commandline, benchmarkoptions, optionReplacementPatterns, passingFileContents = False):
+		benchmarkRunSettings =	{
+			"RunID"						: runID,
+			"Description"				: description,
+			"FlatfileLines"				: None,
+			"RosettaSVNRevision"		: rosettaversion,
+			"RosettaDBSVNRevision"		: rosettadbversion,
+			"RosettaCommandLine"		: commandline,
+			"Options"					: benchmarkoptions,
+			"OptionReplacementPatterns" : optionReplacementPatterns,
+		}
+		benchmarkSettingsKeys = benchmarkRunSettings.keys()
+		for k, v in benchmarkoptions.iteritems():
+			assert(k not in benchmarkSettingsKeys)
+			benchmarkRunSettings[k] = v
+		if flatfile:
+			if not passingFileContents:
+				benchmarkRunSettings["FlatfileLines"] = rosettahelper.readFileLines(flatfile)
+			else:
+				benchmarkRunSettings["FlatfileLines"] = flatfile.split('\n')
+		self.benchmarkRunSettings.append(benchmarkRunSettings)
 		
-	outfile.write('\n\n')
-	outfile.write('%s\n' % str(best_model))
-	outfile.close()
-	eps_outfile_name = "%s_all.eps" % outfile_name.split('.')[0]
-	
-	title = outfile_name.split('/')[-1].split('_')[0]
-	top_X_str = str(top_X)
-	
-	gnuplot_commands='''
-set autoscale
-set border 31
-set tics out
-set terminal postscript eps enhanced color solid "Helvetica" 24
-#set size 1,1.5
-#set size ratio 1
-#set xtics ("default" 1, "default" 2, "H/Y" 3, "Y/H" 4, "default" 6, "default" 7, "H/Y" 8, "Y/H" 9) rotate by -45
-set xtics autofreq
-set xtics nomirror
-set ytics autofreq
-set ytics nomirror
-set noy2tics
-set nox2tics
-
-set style line 1 lt rgb "dark-magenta" lw 2
-set style line 2 lt rgb "blue" lw 2 ps 1 pt 7
-set style line 3 lt rgb "forest-green" lw 2 ps 2 pt 13
-set style line 4 lt rgb "gold" lw 2 ps 1 pt 7
-set style line 5 lt rgb "red" lw 2 ps 2 pt 13
-set style line 6 lt rgb "black" lw 2
-set style line 7 lt rgb "dark-gray" lw 2
-set style line 8 lt rgb "gray" lw 2
-
-set boxwidth 0.75
-
-set key top right
-set xrange [0:]
-set title "%(title)s"
-set encoding iso_8859_1
-set xlabel "r.m.s. deviation to crystal loop [{/E \305}]"
-set ylabel "Rosetta all-atom score"
-set output "%(eps_outfile_name)s"
-plot "%(outfile_name)s" index 0 using ($2):($3) with points ls 2 title "KIC protocol (all models)" axes x1y1, "%(outfile_name)s" index 1 using ($2):($3) with points ls 4 title "KIC protocol (5 lowest energy models)" axes x1y1, "%(outfile_name)s" index 2 using ($2):($3) with points ls 5 title "KIC protocol (top %(top_X_str)s best model)" axes x1y1
-''' % vars()
-	
-	gnuplot_scriptname = outfile_name.split('.')[0] + '.gnu'
-	rosettahelper.writeFile(gnuplot_scriptname, gnuplot_commands)
-	error = statsfns.Popen(outdir, ['/usr/local/bin/gnuplot', gnuplot_scriptname]).getError() # Requires gnuplot 4.2
-	if error:
-		raise Exception(error)
-	error = statsfns.Popen(outdir, ['bash', 'epstopdf','--nocompress', eps_outfile_name]).getError() # The bash is necessary as the epstopdf script does not have a valid shebang which confuses subprocess
-	if error:
-		raise Exception(error)
-
-def densityplot(outdir, sorted_models, outfile_name, num_bins):
-	outfile = open(outfile_name, 'w')
-	outfile.write('#Loop_rmsd\tPercentage_of_models\n')
-	values = [model.loop_rms for model in sorted_models]
-	histogram = statsfns.histogram(values, num_bins)
-	for (bin, num_values) in histogram:
-		outfile.write("%s\t%s\n" % (str(bin), str(100 * num_values/float(len(values)))))
-	outfile.close()
-	
-	eps_outfile_name = "%s.eps" % outfile_name.split('.')[0]
-	title = outfile_name.split('/')[-1].split('_')[0]
-	gnuplot_commands='''
-set autoscale
-set border 31
-set tics out
-set terminal postscript eps enhanced color solid "Helvetica" 24
-#set size 1,1.5
-#set size ratio 1
-#set xtics ("default" 1, "default" 2, "H/Y" 3, "Y/H" 4, "default" 6, "default" 7, "H/Y" 8, "Y/H" 9) rotate by -45
-set xtics autofreq
-set xtics nomirror
-set ytics autofreq
-set ytics nomirror
-set noy2tics
-set nox2tics
-
-set style line 1 lt rgb "dark-magenta" lw 2
-set style line 2 lt rgb "blue" lw 5 ps 1 pt 7
-set style line 3 lt rgb "forest-green" lw 5 ps 2 pt 13
-set style line 4 lt rgb "gold" lw 2 ps 1 pt 7
-set style line 5 lt rgb "red" lw 2 ps 2 pt 13
-set style line 6 lt rgb "black" lw 2
-set style line 7 lt rgb "dark-gray" lw 2
-set style line 8 lt rgb "gray" lw 2
-set style line 9 lt 0 lc rgb "black" lw 5 ps 1 pt 7
-\
-set boxwidth 0.75
-\
-set key top right
-set xrange [0:]
-set title "%(title)s"
-set encoding iso_8859_1
-set xlabel "r.m.s. deviation to crystal loop [{/E \305}]"
-set ylabel "Fraction of models [%%]"
-set output "%(eps_outfile_name)s"
-set arrow from 1,graph 0 to 1,graph 1 nohead ls 9
-plot "%(outfile_name)s" index 0 using ($1):($2) with lines smooth bezier ls 2 title "KIC protocol (all models)" axes x1y1
-''' % vars()
-
-	gnuplot_scriptname = outfile_name.split('.')[0]+'.gnu'
-	rosettahelper.writeFile(gnuplot_scriptname, gnuplot_commands)
-	error = statsfns.Popen(outdir, ['/usr/local/bin/gnuplot', gnuplot_scriptname]).getError() # Requires gnuplot 4.2
-	if error:
-		raise Exception(error)
-	error = statsfns.Popen(outdir, ['bash', 'epstopdf','--nocompress', eps_outfile_name]).getError() # The bash is necessary as the epstopdf script does not have a valid shebang which confuses subprocess
-	if error:
-		raise Exception(error)
-
+	def run(self):
+		if len(self.benchmarkRunSettings) == 1:
+			analysis = SingleRunReport(self.outdir, self.reportsettings, self.benchmarkRunSettings[0], self.quiet, html = self.html)
+		elif len(self.benchmarkRunSettings) == 2:
+			analysis = ComparisonReport(self.outdir, self.reportsettings, self.benchmarkRunSettings[0], self.benchmarkRunSettings[1], quiet = self.quiet, html = self.html)
+		else:
+			raise Exception("Expected either one or two benchmark runs.")
 		
-
-def boxplot(outdir, models, outfile_name):
-	boxplot_data = {}
-	boxplot_data[1] = []
-	for model in models:
-		boxplot_data[1].append(model.loop_rms)
-	#-
-	tuples = statsfns.boxAndWhisker(boxplot_data)
-	outfile = open(outfile_name, 'w')
-	outfile.write('#x\tmin\tfirst_quartile\tmedian\tthird_quartile\tmax\n')
-	for tuple in tuples:
-		for item in tuple:
-			outfile.write(str(item) + '\t')
-		outfile.write('\n\n\n')
-	outfile.close()
-	eps_outfile_name = outfile_name.split('.')[0] + '.eps'
-	gnuplot_commands ='''
-set autoscale
-set border 31
-set tics out
-set terminal postscript eps enhanced color solid "Helvetica" 24
-set size ratio 1
-set xtics ("KIC" 1)
-set xrange [0.5:1.5]
-set nox2tics
-set ytics autofreq
-set ytics nomirror
-set noy2tics
-
-set style line 1 lt rgb "dark-magenta" lw 2
-set style line 2 lt rgb "blue" lw 5 pt 7
-set style line 3 lt rgb "forest-green" lw 2
-set style line 4 lt rgb "gold" lw 2
-set style line 5 lt rgb "red" lw 5 pt 7
-set style line 6 lt rgb "black" lw 5
-set style line 7 lt rgb "dark-gray" lw 2
-set style line 8 lt rgb "gray" lw 2
-
-set boxwidth 0.25
-
-set key tmargin
-set title "Best models performance distribution"
-set noxlabel
-set style fill solid
-set encoding iso_8859_1
-set ylabel "r.m.s. deviation to crystal loop [{/E \305}]"
-set output "%(eps_outfile_name)s"
-plot "%(outfile_name)s" index 0 using 1:3:2:6:5 with candlesticks whiskerbars ls 2 notitle axes x1y1, "%(outfile_name)s"index 0 using 1:4:4:4:4 with candlesticks ls 6 notitle
-''' % vars()
-	gnuplot_scriptname=outfile_name.split('.')[0]+'.gnu'
-	rosettahelper.writeFile(gnuplot_scriptname, gnuplot_commands)
-	error = statsfns.Popen(outdir, ['/usr/local/bin/gnuplot', gnuplot_scriptname]).getError() # Requires gnuplot 4.2
-	if error:
-		raise Exception(error)
-	error = statsfns.Popen(outdir, ['bash', 'epstopdf','--nocompress', eps_outfile_name]).getError()  # The bash is necessary as the epstopdf script does not have a valid shebang which confuses subprocess
-	if error:
-		raise Exception(error)
+		analysis.run()
+		self.PDFReport = analysis.PDFReport
+		return self.PDFReport 
+		
+class BenchmarkAnalyzer(object):
+	placeholder_image='/backrub/daemon/benchmark_kic/data/placeholder_image.eps'
 	
-
-def texHeader():
-	header='''
+	texHeader = '''
 \\documentclass[a4paper,10pt]{article}
+
+\\makeatletter
+\\renewcommand\\paragraph{\\@startsection{paragraph}{4}{\\z@}%
+  {-3.25ex\\@plus -1ex \\@minus -.2ex}%
+  {1.5ex \\@plus .2ex}%
+  {\\normalfont\\normalsize\\bfseries}}
+\\makeatother
+
 \\usepackage{a4wide}
 \\usepackage[utf8]{inputenc}
 \\usepackage[small,bf]{caption}
@@ -238,5 +96,53 @@ def texHeader():
 \\begin{center}
 {\\huge KIC scientific benchmark}\\\\[0.5cm]
 \\today
-\\end{center}'''
-	return header
+\\end{center}
+'''
+	
+	def __init__(self, quiet, html = False):
+		self.quiet = True
+		if html:
+			self.linebreak = "<br>"
+		else:
+			self.linebreak = ""
+		self.PDFReport = None
+		
+	def log(self, *msg):
+		if self.quiet:
+			return
+		if len(msg) == 1:
+			print(msg[0])
+		else:
+			print(msg)
+		print(self.linebreak)
+	
+	def gnuplot(self, gnuplot_commands, gnuplot_scriptname, eps_outfile_name):
+		rosettahelper.writeFile(gnuplot_scriptname, gnuplot_commands)
+		self.popen(['/usr/local/bin/gnuplot', gnuplot_scriptname])  # Requires gnuplot 4.2
+		self.popen(['bash', 'epstopdf','--nocompress', eps_outfile_name]) # The bash is necessary as the epstopdf script does not have a valid shebang which confuses subprocess
+		
+	def popen(self, arglist, logstdout = False, logstderr = False):
+		processOutput = statsfns.Popen(self.outdir, arglist)
+		
+		if logstdout:
+			self.log("STDOUT START")
+			self.log(processOutput.stdout)
+			self.log("STDOUT END")
+		if logstderr:
+			self.log("STDERR START")
+			self.log(processOutput.stderr)
+			self.log("STDERR END")
+		error = processOutput.getError()
+		if error:
+			raise Exception(error)
+
+	def checkFileExists(self, filename):
+		if not os.path.exists(os.path.join(self.outdir, filename)):
+			raise Exception("%s does not exist." % filename)
+
+from reports.SingleRun import SingleRunReport 
+from reports.Comparison import ComparisonReport 
+
+
+	
+
