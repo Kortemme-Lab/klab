542daec91ef6d683f0fc776dc6dcf2d25209023f
Shane O'Connor <shane.oconnor@ucsf.edu>
2013-05-03 12:39:14 -0700

    Updating tools. Separating email functions and starting to separate some file io functions.
    Former-commit-id: b8cbe4991ef0d0009aec1d17b604610b4501a8ef

---
+++ comms/mail.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+mail.py
+For email functions
+"""
+
+from string import join
+import email.Message
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+from tools.fs.io import read_file
+
+class MailServer(object):
+
+    def __init__(self, host = None, port = None):
+        self.host = host
+        self.port = port
+
+    def sendmail(self, subject, sender, recipients, plaintext, htmltext=None, cc=None, debug=False, useMIMEMultipart=True):
+        if recipients:
+            if type(recipients) == type(""):
+                recipients = [recipients]
+            elif type(recipients) != type([]):
+                raise Exception("Unexpected type for recipients.")
+            if cc:
+                if type(cc) == type(""):
+                    recipients.append(cc)
+                elif type(cc) == type([]):
+                    recipients.extend(cc)
+                else:
+                    raise Exception("Unexpected type for cc.")
+            recipients = join(recipients, ";")
+
+            if plaintext and htmltext and useMIMEMultipart:
+                msg = MIMEMultipart('alternative')
+            else:
+                msg = email.Message.Message()
+
+            msg['Subject'] = subject
+            msg['From'] = sender
+            msg['To'] = recipients
+            msg['Reply-To'] = sender
+            if plaintext and htmltext and useMIMEMultipart:
+                part1 = MIMEText(plaintext, 'plain')
+                part2 = MIMEText(htmltext, 'html')
+                msg.attach(part1)
+                msg.attach(part2)
+            else:
+                msg.set_type("text/plain")
+                msg.set_payload(plaintext)
+
+            if debug:
+                print(msg)
+            else:
+                if self.host and self.port:
+                    s = smtplib.SMTP(self.host, self.port)
+                elif self.host:
+                    s = smtplib.SMTP(self.host)
+                else:
+                    s = smtplib.SMTP()
+                s.connect()
+                s.sendmail(msg['From'], recipients, msg.as_string())
+                s.close()
+            return True
+        return False
+
+    def sendgmail(self, subject, recipients, plaintext, htmltext=None, cc=None, debug=False, useMIMEMultipart=True, gmail_account = 'kortemmelab@gmail.com', pw_filepath = None):
+        '''For this function to work, the password for the gmail user must be colocated with this file or passed in.'''
+        smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.ehlo
+        gmail_account = 'kortemmelab@gmail.com'
+        if pw_filepath:
+            smtpserver.login(gmail_account, read_file(pw_filepath))
+        else:
+            smtpserver.login(gmail_account, read_file('pw'))
+        for recipient in recipients:
+            header = 'To:' + recipient + '\n' + 'From: ' + gmail_account + '\n' + 'Subject:' + subject + '\n'
+            msg = header + '\n ' + plaintext + '\n\n'
+            smtpserver.sendmail(gmail_account, recipient, msg)
+        smtpserver.close()
--- fs/stats.py
+++ fs/stats.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+stats.py
+For filesystem statistics functions
+"""
+
 import subprocess
 
 df_conversions = {
@@ -9,11 +16,11 @@ df_conversions = {
 def df(unit = 'GB'):
     '''A wrapper for the df shell command.'''
     details = {}
-    headers = ['Filesystem', 'Size', 'Used', 'Available', 'Use%', 'MountedOn']
+    headers = ['Filesystem', 'Type', 'Size', 'Used', 'Available', 'Use%', 'MountedOn']
     n = len(headers)
 
     unit = df_conversions[unit]
-    p = subprocess.Popen(args = ['df'], stdout = subprocess.PIPE)
+    p = subprocess.Popen(args = ['df', '-T'], stdout = subprocess.PIPE)
     stdout, stderr = p.communicate()
 
     lines = stdout.split("\n")
@@ -30,6 +37,20 @@ def df(unit = 'GB'):
         for x in range(1, len(headers)):
             d[headers[x]] = tokens[x]
         d['Size'] = float(d['Size']) / unit
+        assert(d['Use%'].endswith("%"))
         d['Used'] = float(d['Used']) / unit
+        d['Available'] = float(d['Available']) / unit
+        d['Using'] = 100*(d['Used']/d['Size']) # same as Use% but with more precision
+
+        if d['Type'].startswith('ext'):
+            pass
+            d['Using'] += 5 # ext2, ext3, and ext4 reserve 5% by default
+        else:
+            ext3_filesystems = ['ganon:', 'kortemmelab:', 'albana:']
+            for e3fs in ext3_filesystems:
+                if tokens[0].find(e3fs) != -1:
+                    d['Using'] += 5 # ext3 reserves 5%
+                    break
         details[tokens[0]] = d
+
     return details
