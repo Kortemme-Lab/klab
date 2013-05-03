855f2dfca91f50e810ae3430703f76115c2fbba8
Shane O'Connor <shane.oconnor@ucsf.edu>
2013-05-03 12:39:14 -0700

    Updating tools. Separating email functions and starting to separate some file io functions.

---
+++ fs/io.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# encoding: utf-8
+"""
+io.py
+For common file I/O functions
+"""
+
+def read_file(filepath, binary = False):
+    if binary:
+        output_handle = open(filepath,'rb')
+    else:
+        output_handle = open(filepath,'r')
+    contents = output_handle.read()
+    output_handle.close()
+    return contents
+
+def get_file_lines(filepath):
+    return read_file(filepath, binary = False).splitlines()
+
+def write_file(filepath, contents):
+    output_handle = open(filepath,'w')
+    output_handle.write(contents)
+    output_handle.close()
+
+def open_temp_file(path):
+    F, fname = mkstemp(dir = path)
+    output_handle = os.fdopen(F, "w")
+    return output_handle, fname
+
+def write_temp_file(path, contents):
+    output_handle, fname = open_temp_file(path)
+    output_handle.write(contents)
+    output_handle.close()
+    return fname
