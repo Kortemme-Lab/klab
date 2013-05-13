12223e0a317fc894e533a69b833e96e5c2bf99dc
Kyle Barlow <kb@kylebarlow.com>
2013-05-13 10:34:46 -0700

    Using old version of optparse to run on cluster. Deleting now works

--- cluster/find_owned_files.py
+++ cluster/find_owned_files.py
@@ -3,14 +3,14 @@
 # Obtain version hash from git if possible
 try:
     import subprocess
-    script_version = str(subprocess.check_output(["git", 'rev-parse', '--short', 'HEAD']))
+    script_version = str(subprocess.check_output(["git", 'rev-parse', '--short', 'HEAD'])).strip()
 except Exception:
     script_version = '1.0'
 
-program_description="Script to find files owned by you (or another user) and move them, tar them, and/or delete them. Version "+script_version
+program_description="Script to find files owned by you (or another user) and move them, tar them, and/or delete them. Version %s. One directory positional argument is required."%(script_version)
 
 # Python import statements
-import argparse
+import optparse # For compatibility with pre-2.7 version
 import os
 import getpass
 import time
@@ -28,7 +28,7 @@ class Reporter:
         self.a=0
         self.b=0
         print 'Starting '+task
-    def report(self,a,b):
+    def report(self,a,b=0):
         t=time.time()
         if self.lastreport<(t-report_interval):
             self.lastreport=t
@@ -36,7 +36,10 @@ class Reporter:
             self.b=b
             self.output_report()
     def output_report(self):
-        sys.stdout.write("  Found %d files owned by user out of %d files total\r"%(self.a,self.b))
+        if self.b==0:
+            sys.stdout.write("  Processed files: %d\r"%(self.a))
+        else:
+            sys.stdout.write("  Found %d files owned by user out of %d files total\r"%(self.a,self.b))
         sys.stdout.flush()
     def done(self):
         self.output_report()
@@ -46,11 +49,29 @@ class Reporter:
 def find_owner(f):
     return os.stat(f).st_uid
 
+def delete_files_prompt(num_files):
+    while(True):
+        prompt = raw_input('Delete all %d files? (yes/no): '%(num_files))
+        if prompt == 'yes':
+            return True
+        if prompt == 'no':
+            return False
+        else:
+            print 'Invalid choice, must type "yes" or "no"'
+
+def sizeof_fmt(num):
+    for x in ['bytes','KB','MB','GB']:
+        if num < 1024.0 and num > -1024.0:
+            return "%3.1f%s" % (num, x)
+        num /= 1024.0
+    return "%3.1f%s" % (num, 'TB')
+
 def find_files(input_dir,user):
-    reporter=Reporter('Scanning %s for files owned by userid #%d'%(input_dir,user))
+    reporter=Reporter('scanning %s for files owned by userid #%d'%(input_dir,user))
     total_files_count=0
     owned_files_count=0
     owned_files=[]
+    owned_files_size=long(0)
     for directory_root, directory, filenames in os.walk(input_dir):
         for filename in filenames:
             total_files_count+=1
@@ -61,35 +82,52 @@ def find_files(input_dir,user):
             if owner==user:
                 owned_files_count+=1
                 owned_files.append(full_filename)
-                reporter.report(owned_files_count,total_files_count)
+                owned_files_size+=os.stat(full_filename).st_size
+            reporter.report(owned_files_count,total_files_count)
 
     reporter.done()
-    print owned_files_count,total_files_count
+    print 'Total owned file size: %s\n'%(sizeof_fmt(owned_files_size))
+
+    return owned_files
+
+def delete_files(files):
+    r = Reporter('deleting files')
+    c=0
+    for f in files:
+        os.remove(f)
+        c+=1
+        r.report(c)
+    r.done()
 
 def main():
-    parser = argparse.ArgumentParser(description=program_description,
-                                     fromfile_prefix_chars='@')
-    parser.add_argument('-u','--user',
-                        default=None,
-                        help='User whose ownership is used in search. Argument can be a string username or a numeric user id. Default: current user')
-    parser.add_argument('-d','--delete',
-                        action='store_true',
-                        default=False,
-                        help="Delete files after other tasks complete")
-    parser.add_argument('-m','--move_dir',
-                        default=None,
-                        help="Specify root directory to move files to")
-    parser.add_argument('-t','--tar_file',
-                        default=None,
-                        help="Specify new tar.gz file to save files in")
-    parser.add_argument('input_dir',
-                        help="Directory to recursively scan for files in")
+    parser = optparse.OptionParser(description=program_description,
+                                   version=script_version,
+                                   usage="usage: %prog [options] scan_directory")
+    parser.add_option('-u','--user',
+                      default=None,
+                      help='User whose ownership is used in search. Argument can be a string username or a numeric user id. Default: current user')
+    parser.add_option('-d','--delete',
+                      action='store_true',
+                      default=False,
+                      help="Delete files after other tasks complete")
+    parser.add_option('-m','--move_dir',
+                      default=None,
+                      help="Specify root directory to move files to")
+    parser.add_option('-t','--tar_file',
+                      default=None,
+                      help="Specify new tar.gz file to save files in")
+    #parser.add_option('input_dir',
+    #                  help="Directory to recursively scan for files in")
 
-    args=parser.parse_args()
+    (args,positional_args)=parser.parse_args()
+    if len(positional_args)!=1:
+        print 'ERROR: One directory positional argument is required'
+        sys.exit(1)
+    input_dir=positional_args[0]
 
     # Verify and fill in arguments
-    if not os.path.isdir(args.input_dir) or not os.access(args.input_dir, os.R_OK):
-        print "ERROR: Cannot open directory %s"%(args.input_dir)
+    if not os.path.isdir(input_dir) or not os.access(input_dir, os.R_OK):
+        print "ERROR: Cannot open directory %s"%(input_dir)
         sys.exit(1)
 
     user=args.user
@@ -105,7 +143,10 @@ def main():
                 print 'ERROR: Could not lookup user id for user %s'%(args.user)
                 sys.exit(1)
 
-    files=find_files(args.input_dir,user)
+    files=find_files(input_dir,user)
+
+    if args.delete and delete_files_prompt(len(files)):
+        delete_files(files)
 
 if __name__ == "__main__":
     main()
