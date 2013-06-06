cc4d7e3c4d0bbcdb07a0df2ada9885d6d367bc67
Shane O'Connor <shane.oconnor@ucsf.edu>
2013-06-05 17:14:48 -0700

    Adding caching of UniProt data to disk for the UniProt code to reduce network delays for multiple runs. Added a UniProtACEntry class which parses some of the XML. Overhauled the UniParcEntry class w.r.t. the AC entries.
    This is still work in progress.

--- bio/uniprot.py
+++ bio/uniprot.py
@@ -7,17 +7,21 @@ For functions relating to getting data from UniProt.
 Created by Shane O'Connor 2013
 """
 
+import sys
+import os
 import string
 import urllib,urllib2
 import simplejson
+from xml.dom.minidom import parse, parseString
 
 if __name__ == '__main__':
-    import sys
     sys.path.insert(0, "../..")
 from tools.comms.http import get as http_get
 from tools import colortext
+from tools.hash import CRC64
+from tools.fs.io import read_file, write_file
 
-def uniprot_map(from_scheme, to_scheme, list_of_from_ids):
+def uniprot_map(from_scheme, to_scheme, list_of_from_ids, cache_dir = None):
     '''Maps from one ID scheme to another using the UniProt service.
         list_of_ids should be a list of strings.
         This function was adapted from http://www.uniprot.org/faq/28#id_mapping_examples which also gives examples of
@@ -26,38 +30,62 @@ def uniprot_map(from_scheme, to_scheme, list_of_from_ids):
         an intermediary format like ACC (UniProtKB AC) or ID (UniProtKB ID).
         This function returns a dict mapping the IDs in from_scheme to a list of sorted IDs in to_scheme.
     '''
-    mapping = {}
+
+    full_mapping = {}
+    cached_mapping_file = None
+    if cache_dir:
+        cached_mapping_file = os.path.join(cache_dir, '%s.%s' % (from_scheme, to_scheme))
+        if os.path.exists(cached_mapping_file):
+            full_mapping = simplejson.loads(read_file(cached_mapping_file))
+
     list_of_from_ids = set(list_of_from_ids)
 
-    url = 'http://www.uniprot.org/mapping/'
-    params = {
-        'from'      : from_scheme,
-        'to'        : to_scheme,
-        'format'    : 'tab',
-        'query'     : ' '.join(sorted(list(list_of_from_ids))),
-    }
-    data = urllib.urlencode(params)
-    request = urllib2.Request(url, data)
-    contact = "" # Please set your email address here to help us debug in case of problems.
-    request.add_header('User-Agent', 'Python %s' % contact)
-    response = urllib2.urlopen(request)
-    page = response.read(200000)
-    lines = page.split("\n")
-    assert(lines[-1] == '')
-    assert(lines[0].split("\t") == ['From', 'To'])
-    for line in lines[1:-1]:
-        tokens = line.split("\t")
-        assert(len(tokens) == 2)
-        assert(tokens[0] in list_of_from_ids)
-        mapping[tokens[0]] = mapping.get(tokens[0], [])
-        mapping[tokens[0]].append(tokens[1])
+    requested_mapping = {}
+    remaining_ids = []
+    for id in list_of_from_ids:
+        if full_mapping.get(id):
+            requested_mapping[id] = full_mapping[id]
+        else:
+            remaining_ids.append(id)
+    assert(set(remaining_ids + requested_mapping.keys()) == set(list_of_from_ids))
+
+    if remaining_ids:
+        url = 'http://www.uniprot.org/mapping/'
+        params = {
+            'from'      : from_scheme,
+            'to'        : to_scheme,
+            'format'    : 'tab',
+            'query'     : ' '.join(sorted(list(list_of_from_ids))),
+        }
+        data = urllib.urlencode(params)
+        request = urllib2.Request(url, data)
+        contact = "" # Please set your email address here to help us debug in case of problems.
+        request.add_header('User-Agent', 'Python %s' % contact)
+        response = urllib2.urlopen(request)
+        page = response.read(200000)
+        lines = page.split("\n")
+        assert(lines[-1] == '')
+        assert(lines[0].split("\t") == ['From', 'To'])
+        for line in lines[1:-1]:
+            tokens = line.split("\t")
+            assert(len(tokens) == 2)
+            assert(tokens[0] in list_of_from_ids)
+            full_mapping[tokens[0]] = full_mapping.get(tokens[0], [])
+            full_mapping[tokens[0]].append(tokens[1])
+            requested_mapping[tokens[0]] = requested_mapping.get(tokens[0], [])
+            requested_mapping[tokens[0]].append(tokens[1])
 
     # Sort the IDs
-    for k, v in mapping.iteritems():
+    for k, v in requested_mapping.iteritems():
         assert(len(v) == len(set(v)))
-        mapping[k] = sorted(v)
+        requested_mapping[k] = sorted(v)
+    for k, v in full_mapping.iteritems():
+        assert(len(v) == len(set(v)))
+        full_mapping[k] = sorted(v)
 
-    return mapping
+    if cached_mapping_file:
+        write_file(cached_mapping_file, simplejson.dumps(full_mapping))
+    return requested_mapping
 
 def pdb_to_uniparc(pdb_ids, silent = True):
     '''Returns a mapping {PDB ID -> List(UniParcEntry)}'''
@@ -104,7 +132,7 @@ def pdb_to_uniparc(pdb_ids, silent = True):
             colortext.write("%s: " % pdb_id, "orange")
         m[pdb_id] = []
         for AC in ACs:
-            entry = UniParcEntry(ac_uniparc_mapping[AC], AC, ac_id_mapping[AC])
+            entry = UniParcEntry(ac_uniparc_mapping[AC], [AC], [ac_id_mapping[AC]])
             m[pdb_id].append(entry.to_dict())
             if not silent:
                 colortext.write(".", "green")
@@ -113,15 +141,151 @@ def pdb_to_uniparc(pdb_ids, silent = True):
 
     return m
 
+class UniProtACEntry(object):
+    def __init__(self, UniProtAC, XML = None, cache_dir = None):
+        if cache_dir and not(os.path.exists(cache_dir)):
+            raise Exception("The cache directory %s does not exist." % cache_dir)
+
+        # Get XML
+        if XML == None:
+            protein_xml = None
+            cached_filepath = None
+            if cache_dir:
+                cached_filepath = os.path.join(cache_dir, '%s.xml' % UniProtAC)
+            if os.path.exists(cached_filepath):
+                protein_xml = read_file(cached_filepath)
+            else:
+                url = 'http://www.uniprot.org/uniprot/%s.xml' % UniProtAC
+                protein_xml = http_get(url)
+                if cached_filepath:
+                    write_file(cached_filepath, protein_xml)
+            self.XML = protein_xml
+        else:
+            self.XML = XML
+
+        self.recommended_name = None
+        self.submitted_names = []
+        self.alternative_names = []
+
+        self._dom = parseString(protein_xml)
+        self._parse_sequence_tag()
+        self._parse_protein_tag()
+
+    def _parse_sequence_tag(self):
+        '''Parses the sequence and atomic mass.'''
+        main_tags = self._dom.getElementsByTagName("uniprot")
+        assert(len(main_tags) == 1)
+        entry_tags = main_tags[0].getElementsByTagName("entry")
+        assert(len(entry_tags) == 1)
+        entry_tag = entry_tags[0]
+        # only get sequence tags that are direct children of the entry tag (sequence tags can also be children of entry.comment.conflict)
+        sequence_tags = [child for child in entry_tag.childNodes if child.nodeType == child.ELEMENT_NODE and child.tagName == 'sequence']
+        assert(len(sequence_tags) == 1)
+        sequence_tag = sequence_tags[0]
+
+        # atomic mass, sequence, CRC64 digest
+        self.atomic_mass = float(sequence_tag.getAttribute("mass"))
+        self.sequence = "".join(sequence_tag.firstChild.nodeValue.strip().split("\n"))
+        self.CRC64Digest = sequence_tag.getAttribute("checksum")
+
+    def _parse_protein_tag(self):
+        '''Parses the protein tag to get the names and EC numbers.'''
+
+        protein_nodes = self._dom.getElementsByTagName('protein')
+        assert(len(protein_nodes) == 1)
+        self.protein_node = protein_nodes[0]
+
+        self._get_recommended_name()
+        self._get_submitted_names()
+        self._get_recommended_name()
+        self._get_alternative_names()
+
+    def get_names(self):
+        if self.recommended_name:
+            return [self.recommended_name] + self.alternative_names + self.submitted_names
+        else:
+            return self.alternative_names + self.submitted_names
+
+    @staticmethod
+    def parse_names(tags):
+        names = []
+        for tag in tags:
+            fullNames = tag.getElementsByTagName('fullName')
+            assert(len(fullNames) == 1)
+            fullName = fullNames[0].firstChild.nodeValue
+
+            EC_numbers = []
+            EC_number_tags = tag.getElementsByTagName('ecNumber')
+            for EC_number_tag in EC_number_tags:
+                EC_numbers.append(EC_number_tag.firstChild.nodeValue)
+
+            short_names = []
+            short_name_tags = tag.getElementsByTagName('shortName')
+            for short_name_tag in short_name_tags:
+                short_names.append(short_name_tag.firstChild.nodeValue)
+
+            names.append({'Name' : fullName, 'EC numbers' : EC_numbers, 'Short names' : EC_numbers})
+        return names
+
+    def _get_recommended_name(self):
+        # only get recommendedName tags that are direct children of the protein tag (recommendedName tags can also be children of protein.component tags)
+        recommended_names = [child for child in self.protein_node.childNodes if child.nodeType == child.ELEMENT_NODE and child.tagName == 'recommendedName']
+        if recommended_names:
+            assert(len(recommended_names) == 1)
+            recommended_names = UniProtACEntry.parse_names(recommended_names)
+            assert(len(recommended_names) == 1)
+            self.recommended_name = recommended_names[0]
+
+    def _get_submitted_names(self):
+        submitted_names = self.protein_node.getElementsByTagName('submittedName')
+        if submitted_names:
+            for submitted_name in submitted_names:
+                # According to the schema (http://www.uniprot.org/docs/uniprot.xsd), submitted names have no short names
+                assert(len(submitted_name.getElementsByTagName('shortName')) == 0)
+            self.submitted_names = UniProtACEntry.parse_names(submitted_names)
+
+    def _get_alternative_names(self):
+        alternative_names = self.protein_node.getElementsByTagName('alternativeName')
+        if alternative_names:
+             self.alternative_names = UniProtACEntry.parse_names(alternative_names)
+
+
 class UniParcEntry(object):
 
-    def __init__(self, UniParcID, UniProtAC, UniProtKB):
-        self.UniParcID = UniParcID
-        self.UniProtAC = UniProtAC
-        self.UniProtKB = UniProtKB
+    def __init__(self, UniParcID, UniProtACs = None, UniProtKBs = None, cache_dir = None):
+        if cache_dir and not(os.path.exists(os.path.abspath(cache_dir))):
+            raise Exception("The cache directory %s does not exist." % os.path.abspath(cache_dir))
 
-        url = 'http://www.uniprot.org/uniparc/%s.fasta' % UniParcID
-        fasta = http_get(url)
+        self.UniParcID = UniParcID
+        self.cache_dir = cache_dir
+
+        # Get AC mapping
+        if not UniProtACs:
+            mapping = uniprot_map('UPARC', 'ACC', [UniParcID], cache_dir = cache_dir)[UniParcID]
+            self.UniProtACs = mapping
+        else:
+            self.UniProtACs = UniProtACs
+
+        # Get ID mapping
+        if not UniProtKBs:
+            mapping = uniprot_map('UPARC', 'ID', [UniParcID], cache_dir = cache_dir)[UniParcID]
+            self.UniProtKBs = mapping
+        else:
+            self.UniProtKBs = UniProtKBs
+
+        # Get FASTA
+        cached_filepath = None
+        if cache_dir:
+            cached_filepath = os.path.join(cache_dir, '%s.fasta' % UniParcID)
+        if os.path.exists(cached_filepath):
+            fasta = read_file(cached_filepath)
+        else:
+            url = 'http://www.uniprot.org/uniparc/%s.fasta' % UniParcID
+            fasta = http_get(url)
+            if cached_filepath:
+                write_file(cached_filepath, fasta)
+
+        # Get sequence
         header = fasta.split("\n")[0].split()
         assert(len(header) == 2)
         assert(header[0] == ">%s" % UniParcID)
@@ -129,12 +293,42 @@ class UniParcEntry(object):
         sequence = "".join(map(string.strip, fasta.split("\n")[1:]))
         self.sequence = sequence
 
+        # Get atomic mass (and sequence again)
+        self.atomic_mass = None
+        self.CRC64Digest = None
+        recommended_names = set()
+        for UniProtAC in self.UniProtACs:
+            colortext.write("%s\n" % UniProtAC, "cyan")
+            AC_entry = UniProtACEntry(UniProtAC, cache_dir = self.cache_dir)
+
+            # Mass sanity check
+            if self.atomic_mass != None:
+                assert(self.atomic_mass == AC_entry.atomic_mass)
+            self.atomic_mass = AC_entry.atomic_mass
+
+            # Sequence sanity check
+            assert(self.sequence == AC_entry.sequence)
+
+            # CRC 64 sanity check
+            if self.CRC64Digest != None:
+                assert(self.CRC64Digest == AC_entry.CRC64Digest)
+            self.CRC64Digest = AC_entry.CRC64Digest
+            assert(CRC64.CRC64digest(self.sequence) == self.CRC64Digest)
+
+            if AC_entry.recommended_name:
+                recommended_names.add(AC_entry.recommended_name['Name'])
+            #print(AC_entry.get_names())
+        print('recommended_names', recommended_names)
+
+
     def to_dict(self):
         return {
             'UniParcID' : self.UniParcID,
             'UniProtAC' : self.UniProtAC,
             'UniProtKB' : self.UniProtKB,
             'sequence'  : self.sequence,
+            'atomic_mass'  : self.atomic_mass,
+            'CRC64Digest'  : self.CRC64Digest,
         }
 
     def __repr__(self):
