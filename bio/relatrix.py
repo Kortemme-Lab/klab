706d2053b4dd08839acc1d56135f663c27f8845d
Shane O'Connor <guybrush.ucsf.edu>
2013-09-18 19:04:44 -0700

    Updated the relatrix class. We now have all the sequences and are only missing SequenceMaps and a convert function to tie everything together.
    Former-commit-id: 59f1c57b3949a12015fa7008b11742405abbb802

--- bio/relatrix.py
+++ bio/relatrix.py
@@ -17,9 +17,12 @@ from tools import colortext
 
 class ResidueRelatrix(object):
 
-    def __init__(self, pdb_id, chains_to_keep = [], min_clustal_cut_off = 80, cache_dir = None, silent = False): # keep_HETATMS = False
+    def __init__(self, pdb_id, rosetta_scripts_path, rosetta_database_path, chains_to_keep = [], min_clustal_cut_off = 80, cache_dir = None, silent = False): # keep_HETATMS = False
+
         self.pdb_id = pdb_id
         self.silent = silent
+        self.rosetta_scripts_path = rosetta_scripts_path
+        self.rosetta_database_path = rosetta_database_path
 
         self.FASTA = None
         self.pdb = None
@@ -29,8 +32,77 @@ class ResidueRelatrix(object):
         self.alignment_cutoff = None
 
         self._create_objects(chains_to_keep, min_clustal_cut_off, True, cache_dir) # todo: at present, we always strip HETATMs. We may want to change this in the future.
+        self._create_sequences()
+        self._create_sequence_maps()
+        self._validate()
 
     ### Private methods ###
+
+    def _validate(self):
+        '''Get all of the SequenceMaps - Rosetta->ATOM, ATOM->SEQRES/FASTA, SEQRES->UniParc.'''
+
+        ## Make sure the domains and ranges of the SequenceMaps match the Sequences
+
+        # rosetta_to_atom_sequence_maps
+        for chain_id, sequence_map in self.rosetta_to_atom_sequence_maps.iteritems():
+            # Check that all Rosetta residues have a mapping
+            assert(sorted(sequence_map.keys()) == sorted(self.rosetta_sequences[chain_id].ids()))
+
+            # Check that all ATOM residues in the mapping exist and that the mapping is injective
+            rng = set(sequence_map.values())
+            atom_ids = set(self.atom_sequences[chain_id].ids())
+            assert(rng.intersection(set(self.atom_sequences[chain_id].ids())) == rng)
+            assert(len(rng) == len(sequence_map.values()))
+
+        # atom_to_seqres_sequence_maps
+        #todo self.atom_to_seqres_sequence_maps
+
+        # seqres_to_uniparc_sequence_maps
+        #todo self.seqres_to_uniparc_sequence_maps
+
+
+        #todo: make sure all the residue types map through translation
+
+    def _create_sequence_maps(self):
+        '''Get all of the SequenceMaps - Rosetta->ATOM, ATOM->SEQRES/FASTA, SEQRES->UniParc.'''
+
+        self.rosetta_to_atom_sequence_maps = self.pdb.rosetta_to_atom_sequence_maps
+        #todo self.atom_to_seqres_sequence_maps = self.pdb.atom_to_seqres_sequence_maps
+        #todo self.seqres_to_uniparc_sequence_maps = ???
+
+        #todo: make sure all the residue types map through translation
+
+    def _create_sequences(self):
+        '''Get all of the Sequences - Rosetta, ATOM, SEQRES, FASTA, UniParc.'''
+
+        # Create the Rosetta sequences and the maps from the Rosetta sequences to the ATOM sequences
+        self.pdb.construct_pdb_to_rosetta_residue_map(self.rosetta_scripts_path, self.rosetta_database_path)
+
+        # Get all the Sequences
+        self.uniparc_sequences = self.PDB_UniParc_SA.uniparc_sequences
+        self.fasta_sequences = self.FASTA.get_sequences(self.pdb_id)
+        self.seqres_sequences = self.pdb.seqres_sequences
+        self.atom_sequences = self.pdb.atom_sequences
+        self.rosetta_sequences = self.pdb.rosetta_sequences
+
+        # Update the chain types for the UniParc sequences
+        uniparc_pdb_chain_mapping = {}
+        for pdb_chain_id, matches in self.PDB_UniParc_SA.clustal_matches.iteritems():
+            uniparc_chain_id = matches.keys()[0]
+            assert(len(matches) == 1)
+            uniparc_pdb_chain_mapping[uniparc_chain_id] = uniparc_pdb_chain_mapping.get(uniparc_chain_id, [])
+            uniparc_pdb_chain_mapping[uniparc_chain_id].append(pdb_chain_id)
+        for uniparc_chain_id, pdb_chain_ids in uniparc_pdb_chain_mapping.iteritems():
+            sequence_type = set([self.seqres_sequences[p].sequence_type for p in pdb_chain_ids])
+            assert(len(sequence_type) == 1)
+            sequence_type = sequence_type.pop()
+            assert(self.uniparc_sequences[uniparc_chain_id].sequence_type == None)
+            self.uniparc_sequences[uniparc_chain_id].set_type(sequence_type)
+
+        # Update the chain types for the FASTA sequences
+        for chain_id, sequence in self.seqres_sequences.iteritems():
+            self.fasta_sequences[chain_id].set_type(sequence.sequence_type)
+
 
     def _create_objects(self, chains_to_keep, min_clustal_cut_off, strip_HETATMS, cache_dir):
 
@@ -41,7 +113,7 @@ class ResidueRelatrix(object):
         if not self.silent:
             colortext.message("Creating the FASTA object.")
         try:
-            self.FASTA = FASTA.retrieve(pdb_id, cache_dir = cache_dir)[pdb_id]
+            self.FASTA = FASTA.retrieve(pdb_id, cache_dir = cache_dir)
         except:
             raise colortext.Exception("Relatrix construction failed creating the FASTA object for %s.\n%s" % (pdb_id, traceback.format_exc()))
 
@@ -58,7 +130,7 @@ class ResidueRelatrix(object):
 
         # Check that the FASTA and SEQRES sequences agree (they sometimes differ)
         for chain_id, sequence in self.pdb.seqres_sequences.iteritems():
-            if sequence != self.FASTA[chain_id]:
+            if str(sequence) != self.FASTA[pdb_id][chain_id]:
                 raise colortext.Exception("The SEQRES and FASTA sequences disagree for chain %s in %s. This can happen but special-case handling should be added to the file containing the %s class." % (chain_id, pdb_id, self.__class__.__name__))
 
         # Create the PDBML object
@@ -96,5 +168,3 @@ class ResidueRelatrix(object):
             raise colortext.Exception("Relatrix construction failed creating the PDBUniParcSequenceAligner object for %s. The cut-off level reached %d without finding a match for all chains but at that level, the mapping from chains to UniParc IDs was not injective.\n%s" % (pdb_id, cut_off, str(e)))
         except:
             raise colortext.Exception("Relatrix construction failed creating the PDBUniParcSequenceAligner object for %s.\n%s" % (pdb_id, traceback.format_exc()))
-
-        print(self.PDB_UniParc_SA.clustal_matches)
