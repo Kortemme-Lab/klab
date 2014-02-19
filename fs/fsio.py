250abc2397a434631d9a18eedca32486499d60c4
Shane O'Connor <oconchus@guybrush.ucsf.edu>
2014-02-18 20:43:56 -0800

    Temp add.

--- fs/fsio.py
+++ fs/fsio.py
@@ -10,14 +10,17 @@ Created by Shane O'Connor 2013
 import os
 import tempfile
 import gzip
+import stat
 
 # Note: I should use the same convention for all methods here but read_file differs. We should really support the whole fopen cstdio spec.
 
+permissions755SGID = stat.S_ISGID | stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH
+
 def read_file(filepath, binary = False):
     if binary:
-        output_handle = open(filepath,'rb')
+        output_handle = open(filepath, 'rb')
     else:
-        output_handle = open(filepath,'r')
+        output_handle = open(filepath, 'r')
     contents = output_handle.read()
     output_handle.close()
     return contents
@@ -40,6 +43,17 @@ def write_temp_file(path, contents, ftype = 'w'):
     output_handle.write(contents)
     output_handle.close()
     return fname
+
+def create_temp_755_path(temproot, suffix = None):
+    if suffix:
+        path = tempfile.mkdtemp("_%s" % suffix, dir = temproot)
+    else:
+        path = tempfile.mkdtemp(dir = temproot)
+    if not os.path.isdir(path):
+        raise os.error
+    global permissions755SGID
+    os.chmod(path, permissions755SGID)
+    return path
 
 def safe_gz_unzip(contents):
     ''' Takes a file's contents passed as a string (contents) and either gz-unzips the contents and returns the uncompressed data or else returns the original contents.
