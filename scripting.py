2263cb3966e75a43d5a451b508ec9b812e1f8dc3
Kale Kundert <kale@thekunderts.net>
2014-10-10 16:49:34 -0700

    Create a collection of useful scripting commands.
    Former-commit-id: f82b058c267fef1a2ca6e26654b00262892efa44

---
+++ scripting.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+import os, shutil
+
+def print_warning(message, *args, **kwargs):
+    import colortext
+    if args or kwargs: message = message.format(*args, **kwargs)
+    colortext.write(message, color='red')
+
+def print_error_and_die(message, *args, **kwargs):
+    print_warning(message + "  Aborting...", *args, **kwargs)
+    raise SystemExit(1)
+
+def clear_directory(directory):
+    if os.path.exists(directory): shutil.rmtree(directory)
+    os.makedirs(directory)
+
