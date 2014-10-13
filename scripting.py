e22360f13fd3b582e7b0898549f656a76a038306
Kale Kundert <kale@thekunderts.net>
2014-10-12 22:12:00 -0700

    Fix a formatting error in print_error_and_die().

--- scripting.py
+++ scripting.py
@@ -5,7 +5,7 @@ import os, shutil
 def print_warning(message, *args, **kwargs):
     import colortext
     if args or kwargs: message = message.format(*args, **kwargs)
-    colortext.write(message, color='red')
+    colortext.write(message + '\n', color='red')
 
 def print_error_and_die(message, *args, **kwargs):
     print_warning(message + "  Aborting...", *args, **kwargs)
