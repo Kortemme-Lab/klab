99ff5ecb3b43b2362e1cf6ec9774e37a3916745e
Kale Kundert <kale@thekunderts.net>
2014-10-17 12:56:58 -0700

    Add a few more scripting utilities.

--- scripting.py
+++ scripting.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
-import os, shutil
+import os, shutil, glob
+from contextlib import contextmanager
 
 def print_warning(message, *args, **kwargs):
     import colortext
@@ -11,9 +12,35 @@ def print_error_and_die(message, *args, **kwargs):
     print_warning(message + "  Aborting...", *args, **kwargs)
     raise SystemExit(1)
 
+def use_path_completion():
+    import readline
+    readline.set_completer_delims(' \t\n;')
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(path_completer)
+
+def path_completer(text, state):
+    globs = glob.glob(os.path.expanduser(text) + '*') + [None]
+    add_slash = lambda x: x + '/' if os.path.isdir(x) else x
+    return add_slash(globs[state])
+
 def clear_directory(directory):
     if os.path.exists(directory): shutil.rmtree(directory)
     os.makedirs(directory)
+
+def run_main(locals):
+    try:
+        if locals['__name__'] == '__main__':
+            locals['main']()
+
+    except KeyboardInterrupt:
+        print
+
+    except Exception as error:
+        if hasattr(error, 'no_stack_trace'): print_warning(error)
+        else: raise
+
+
+# Bread'n'butter shell commands.
 
 def mkdir(newdir):
     if os.path.isdir(newdir):
@@ -23,3 +50,7 @@ def mkdir(newdir):
                       "dir, '%s', already exists." % newdir)
     else:
         os.makedirs(newdir)
+
+def touch(path):
+    with open(path, 'w'):
+        pass
