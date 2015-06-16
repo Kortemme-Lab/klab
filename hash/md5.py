ff24c2b1b34b86a64dd6c45e8277ef0f53aac1be
Shane O'Connor <spiderbaby@gmail.com>
2015-06-16 16:25:40 -0700

    Adding a wrapper for the MD5 hash/digest function.

---
+++ hash/md5.py
@@ -0,0 +1,11 @@
+import hashlib
+from tools.general.strutil import remove_trailing_line_whitespace
+
+def get_hexdigest(content, rm_trailing_line_whitespace = False):
+    if rm_trailing_line_whitespace:
+        content = remove_trailing_line_whitespace(content)
+    md5hash = hashlib.md5()
+    md5hash.update(content)
+    hexdigest = md5hash.hexdigest()
+    assert(len(hexdigest) == 32)
+    return hexdigest
