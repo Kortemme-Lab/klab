ba908c2db4f0adc0b7298c6dab52a18efd10d571
Shane O'Connor <spiderbaby@gmail.com>
2015-09-04 19:39:45 -0700

    Updating Bonzai. There is some functionality added but this is really a midway commit while I revert to check something out.

--- bio/bonzai.py
+++ bio/bonzai.py
@@ -34,13 +34,101 @@ import rcsb
 from tools.general.strutil import remove_trailing_line_whitespace as normalize_pdb_file
 
 
+backbone_atoms = set(['N', 'CA', 'C', 'O'])
+
+
+class Residue(object):
+
+
+    def __init__(self):
+        self.records = dict(ATOM = [], HETATM = [], ANISOU = [])
+
+
+    def add(self, record_type, atom):
+        self.records[record_type].append(atom)
+
+
+    def get(self, record_type):
+        return self.records[record_type]
+
+
+    def __repr__(self):
+        return pprint.pformat(self.records)
+
+
+
+class Atom(object):
+
+
+    def __init__(self, residue, name, group, serial_number):
+        self.residue = residue
+        self.x, self.y, self.z = None, None, None
+        self.point = None
+        self.serial_number = serial_number
+        self.name = name
+        self.group = group
+        self.bin = None
+        self.record_type = None
+
+
+    def place(self, x, y, z, record_type = None):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.record_type = record_type
+        self.point = numpy.array([x, y, z])
+
+
+    def set_bin(self, bin):
+        assert(self.bin == None or self.bin == bin)
+        self.bin = bin
+
+
+    def __sub__(self, other):
+        '''Returns the distance (Euclidean/Frobenius norm) between this point and the other point.'''
+        return numpy.linalg.norm(self.point - other.point)
+
+
+    def __repr__(self):
+        if self.point is None:
+            return '{0} {1}'.format(self.name, self.serial_number)
+        else:
+            return '{0}: {1} {2} at ({3}, {4}, {5})'.format(self.record_type, self.name, self.serial_number, self.x, self.y, self.z)
+
+
+class Bin(object):
+
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+        self.items = []
+
+    def append(self, i):
+        self.items.append(i)
+
+    def __len__(self):
+        return len(self.items)
+
+    def __iter__(self):
+        return self.items.__iter__()
+
+
 class Bonzai(object):
 
 
     ### Constructors
 
-    def __init__(self, pdb_content, buffer = 0.05, bin_size = 5.1):
-        '''Takes either a pdb file, a list of strings = lines of a pdb file, or another object.'''
+    def __init__(self, pdb_content, buffer = 0.05, bin_size = 5.1, safe_mode = True):
+        '''Takes either a pdb file, a list of strings = lines of a pdb file, or another object.
+           safe_mode checks to make sure that certain assertions holds but adds to the runtime.
+
+           residues only contains residue details for ATOM, HETATM, and ANISOU records.
+           atoms only contains ATOM or HETATM records.
+           indexed_lines is a document-order tagged list of PDB file lines
+           Each item in the list is one of three types:
+               ATOM and HETATM records correspond to a quadruple (record_type, serial_number, line, new_atom)
+               ANISOU records correspond to a triple (record_type, serial_number, line)
+               Other lines correspond to a double (None, line)
+        '''
 
         self.pdb_content = pdb_content
         self.lines = pdb_content.split("\n")
@@ -50,8 +138,12 @@ class Bonzai(object):
         self.atom_bins = None
         self.indexed_lines = None
         self.atom_bin_dimensions = None
+        self.atom_name_to_group = {}
+        self.safe_mode = safe_mode
         self.parse()
         self.bin_atoms()
+        if self.safe_mode:
+            self.check_residues()
 
 
     @staticmethod
@@ -93,7 +185,9 @@ class Bonzai(object):
 
 
     def parse(self, lines = None):
-        '''Builds a quadtree and an indexed representation of the PDB file.
+        '''Parses the PDB file into a indexed representation (a tagged list of lines, see constructor docstring).
+           A set of Atoms is created, including x, y, z coordinates when applicable. These atoms are grouped into Residues.
+           Finally, the limits of the 3D space are calculated to be used for Atom binning.
 
            ATOM serial numbers appear to be sequential within a model regardless of alternate locations (altLoc, see 1ABE). This
            code assumes that this holds as this serial number is used as an index.
@@ -112,19 +206,30 @@ class Bonzai(object):
         # atoms maps chain -> residue IDs to Residue objects. Residue objects remember which ATOM/HETATM/ANISOU records (stored as Atom objects) belong to them
         residues = {}
 
+        atom_name_to_group = {}
         for line in self.lines:
             record_type = line[:6].strip()
             if record_type in removable_records_types_with_atom_serial_numbers:
 
                 #altLoc = line[16]
-                atom_name = line[12:16]
+                atom_name = line[12:16].strip()
                 chain = line[21]
                 resid = line[22:27] # residue ID + insertion code
                 serial_number = int(line[6:11])
+                atom_group = None
+                if record_type == 'ATOM':
+                    atom_group = atom_name_to_group.get(atom_name)
+                    if not atom_group:
+                        # The convention seems to be that the first alphabetic character is the group
+                        for c in atom_name:
+                            if c.isalpha():
+                                atom_name_to_group[atom_name] = c
+                                atom_group = c
+                                break
 
                 residues[chain] = residues.get(chain, {})
                 residues[chain][resid] = residues[chain].get(resid, Residue())
-                new_atom = Atom(residues[chain][resid], atom_name, serial_number)
+                new_atom = Atom(residues[chain][resid], atom_name, atom_group, serial_number)
                 residues[chain][resid].add(record_type.strip(), new_atom)
 
                 if record_type in removable_xyz_records_types_with_atom_serial_numbers:
@@ -134,7 +239,7 @@ class Bonzai(object):
                     zs.append(z)
                     assert(serial_number not in atoms) # the logic of this class relies on this assertion - that placed records have a unique identifier
                     atoms[serial_number] = new_atom
-                    atoms[serial_number].place(x, y, z)
+                    atoms[serial_number].place(x, y, z, record_type)
                     indexed_lines.append((record_type, serial_number, line, new_atom))
                 else:
                     indexed_lines.append((record_type, serial_number, line))
@@ -155,9 +260,11 @@ class Bonzai(object):
         self.residues = residues
         self.atoms = atoms
         self.indexed_lines = indexed_lines
+        self.atom_name_to_group = atom_name_to_group
 
 
     def bin_atoms(self):
+        '''This function bins the Atoms into fixed-size sections of the protein space in 3D.'''
 
         # Create the atom bins
         low_point = numpy.array([self.min_x, self.min_y, self.min_z])
@@ -180,17 +287,46 @@ class Bonzai(object):
             atom.set_bin(bin)
 
         # Sanity_check
-        num_atoms = 0
-        for x in range(int(atom_bin_dimensions[0])):
-            for y in range(int(atom_bin_dimensions[1])):
-                for z in range(int(atom_bin_dimensions[2])):
-                    num_atoms += len(atom_bins[x][y][z])
-        assert(num_atoms == len(self.atoms))
+        if self.safe_mode:
+            num_atoms = 0
+            for x in range(int(atom_bin_dimensions[0])):
+                for y in range(int(atom_bin_dimensions[1])):
+                    for z in range(int(atom_bin_dimensions[2])):
+                        num_atoms += len(atom_bins[x][y][z])
+            assert(num_atoms == len(self.atoms))
 
         self.atom_bins = atom_bins
 
 
-    def find_atoms_near_atom(self, source_atom, search_radius):
+    def find_heavy_atoms_near_atom(self, source_atom, search_radius):
+        non_heavy_atoms = self.get_atom_names_by_group(set('H', 'D', 'T'))
+        print(non_heavy_atoms)
+        sys.exit(0)
+        return self.find_atoms_near_atom(source_atom, search_radius, atom_names_to_exclude = non_heavy_atoms)
+
+
+    def get_atom_names_by_group(self, groups):
+        names = set()
+        groups = set(groups)
+        for nm, g in self.atom_name_to_group.iteritems():
+            if g in groups:
+                names.add(nm)
+        return names
+
+
+    def find_atoms_near_atom(self, source_atom, search_radius, atom_names_to_include = set(), atom_names_to_exclude = set()):
+
+        atom_names_to_keep = set()
+        if len(atom_names_to_include) > 0 and len(atom_names_to_exclude) > 0:
+            raise Exception('Error: either one of the set of atoms types to include or the set of atom types to exclude can be set but not both.')
+        if atom_names_to_include:
+            atom_names_to_keep = set(atom_names_to_include)
+        else:
+            atom_names_to_keep = set(self.atom_name_to_group.keys())
+            atom_names_to_exclude = set(atom_names_to_exclude)
+            atom_names_to_keep = atom_names_to_keep.difference(atom_names_to_exclude)
+        print(atom_names_to_keep)
+
         radius = float(search_radius) + self.buffer # add buffer to account for edge cases in searching
         bin_size = self.bin_size
         atom_bins = self.atom_bins
@@ -205,6 +341,11 @@ class Bonzai(object):
                     for z in zrange:
                         for atom in atom_bins[x][y][z]:
                             if source_atom - atom <= search_radius:
+                                if atom.name not in atom_names_to_keep:
+                                    print(atom)
+                                hits.append(atom)
+                            continue
+                            if (source_atom - atom <= search_radius) and (atom.name in atom_names_to_keep):
                                 hits.append(atom)
             return hits
 
@@ -226,102 +367,58 @@ class Bonzai(object):
         return complement
 
 
+    def prune(self, atoms):
+        '''Returns two PDB objects.
+           The first object is missing the ATOM (and any related ANISOU) and HETATM records identified by atoms.
+           The second object only contains ATOM, ANISOU, and HETATM records which are identified by atoms.
+           Both PDB objects contain all records from the original PDB which are not ATOM, ANISOU, or HETATM records.'''
+
 #(None, 'REMARK 280  (PH 7.0). LARGE CRYSTALS (0.2 X 0.2 X 0.5 MM) WERE GROWN BY         ')
 #('ATOM', 3212, 'ATOM   3212  CB  ASP A 397      47.343  47.896  35.182  1.00 31.31           C  ', CB 3212 at (47.343, 47.896, 35.182))
 #('HETATM', 4082, 'HETATM 4082  O   HOH A1422      59.725  49.320  51.258  1.00 68.31           O  ', O 4082 at (59.725, 49.32, 51.258))
 #('ANISOU', 4082, 'ANISOU 4082  O   HOH A1422     8865   8777   8311    969   1113   -375       O  ')
-
-    def build_quadtree(self, radius_):
-
-        residue_atoms = residues['A'][' 210 '].get('ATOM')
-        atom_range = len(residue_atoms)
-        for x in range(0, atom_range - 1):
-            for y in range(x + 1, atom_range):
-                print(x, y, residue_atoms[x], residue_atoms[y], residue_atoms[x] - residue_atoms[y])
+        pass
 
 
-
-class Residue(object):
-
-
-    def __init__(self):
-        self.records = dict(ATOM = [], HETATM = [], ANISOU = [])
+    ### Safety checks
 
 
-    def add(self, record_type, atom):
-        self.records[record_type].append(atom)
+    def check_residues(self):
+        '''Checks to make sure that each atom type is unique per residue.'''
+        for chain, residue_ids in self.residues.iteritems():
+            for residue_id, residue in residue_ids.iteritems():
+                for record_type, atoms in residue.records.iteritems():
+                    freq = {}
+                    for atom in atoms:
+                        freq[atom.name] = freq.get(atom.name, 0)
+                        freq[atom.name] += 1
+                    for atom_type, count in freq.items():
+                        if count > 1:
+                            raise Exception('{0} occurrences of atom type {1} for record type {2} occur in residue {3} in chain {4}.'.format(count, atom_type, record_type, residue_id.strip(), chain))
 
 
-    def get(self, record_type):
-        return self.records[record_type]
-
-
-    def __repr__(self):
-        return pprint.pformat(self.records)
-        #atom 1- residue A 1-452, 501,502,1001-1424
-
-
-
-class Atom(object):
-
-
-    def __init__(self, residue, name, serial_number):
-        self.residue = residue
-        self.x, self.y, self.z = None, None, None
-        self.point = None
-        self.serial_number = serial_number
-        self.name = name.strip()
-        self.bin = None
-
-
-    def place(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.point = numpy.array([x, y, z])
-
-
-    def set_bin(self, bin):
-        assert(self.bin == None or self.bin == bin)
-        self.bin = bin
-
-
-    def __sub__(self, other):
-        '''Returns the distance (Euclidean/Frobenius norm) between this point and the other point.'''
-        return numpy.linalg.norm(self.point - other.point)
-
-
-    def __repr__(self):
-        if self.point is None:
-            return '{0} {1}'.format(self.name, self.serial_number)
-        else:
-            return '{0} {1} at ({2}, {3}, {4})'.format(self.name, self.serial_number, self.x, self.y, self.z)
-
-
-class Bin(object):
-
-    def __init__(self, x, y, z):
-        self.x, self.y, self.z = x, y, z
-        self.items = []
-
-    def append(self, i):
-        self.items.append(i)
-
-    def __len__(self):
-        return len(self.items)
-
-    def __iter__(self):
-        return self.items.__iter__()
 
 
 if __name__ == '__main__':
 
-    b = Bonzai.retrieve('1a8d', cache_dir='/tmp')
+    # 1a8d is an example from the loops benchmark
+    # 1lfa contains hydrogens
+
+    #b = Bonzai.retrieve('1a8d', cache_dir='/tmp')
+
+    b = Bonzai.retrieve('1lfa', cache_dir='/tmp')
     search_radius = 10.0
     atom_of_interest = b.get_atom(1095)
+    print(atom_of_interest)
     nearby_atoms = b.find_atoms_near_atom(atom_of_interest, search_radius)
     for na in nearby_atoms:
         assert(na - atom_of_interest <= search_radius)
+    print(len(nearby_atoms))
+    sys.exit(0)
+
+    print(nearby_atoms)
     for fa in b.get_atom_set_complement(nearby_atoms):
         assert(fa - atom_of_interest > search_radius)
+    nearby_atoms = b.find_heavy_atoms_near_atom(atom_of_interest, search_radius)
+    nearby_ca_atoms = b.find_atoms_near_atom(atom_of_interest, search_radius, atom_names_to_include = ['CA'])
 
