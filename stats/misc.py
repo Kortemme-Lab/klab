e9aee63331b43849a41504907f09150351af03eb
Shane O'Connor <spiderbaby@gmail.com>
2015-10-16 21:16:23 -0700

    Fixing merge issue.

--- stats/misc.py
+++ stats/misc.py
@@ -7,6 +7,8 @@ A place for miscellaneous statistical functions to live until there is a suitabl
 Created by Shane O'Connor 2014
 """
 
+import math
+from scipy.stats import pearsonr, spearmanr, normaltest, ks_2samp, kstest, norm
 from tools.unmerged.rpache.functions_lib import gammaCC
 
 
@@ -35,6 +37,76 @@ def fraction_correct(x_values, y_values, x_cutoff = 1.0, y_cutoff = 1.0):
     return correct / float(num_points)
 
 
+def fraction_correct_fuzzy_linear(x_values, y_values, x_cutoff = 1.0, x_fuzzy_range = 0.1, y_scalar = 1.0):
+    '''A version of fraction_correct which is more forgiving at the boundary positions.
+       In fraction_correct, if the x value is 1.01 and the y value is 0.99 (with cutoffs of 1.0) then that pair evaluates
+       to zero despite the results being very close to each other.
+       This function corrects for the boundary by overlapping the ranges and attenuating the endpoints.
+       This version of the function uses a linear approach - a classification (positive, negative, neutral resp. P, N, X)
+       is 1 for some range of values, 0 for a separate range, and between 0 and 1 for the in between range i.e.
+           N       X        P
+          ----\  /----\  /-----
+               \/      \/
+               /\      /\
+          ----/  \----/  \----
+       This approach was suggested by Kale Kundert.
+    '''
+    num_points = len(x_values)
+    assert(num_points == len(y_values))
+    correct = 0.0
+    for i in range(num_points):
+        x = x_values[i]
+        y = y_values[i]
+
+        #N X P
+        xvec = [0, 0, 0]
+
+        xvec = numpy.dot([0.9, 0.1, 0.0], [0.9, 0.1, 0.0])
+        if (x >= x_cutoff + x_fuzzy_range):
+            xvec = [0, 0, 1]
+        elif (x <= -x_cutoff - x_fuzzy_range):
+            xvec = [1, 0, 0]
+        elif (-x_cutoff - x_fuzzy_range <= x <= x_cutoff - x_fuzzy_range):
+            xvec = [0, 1, 0]
+        elif (-x_cutoff - x_fuzzy_range > x > -x_cutoff + x_fuzzy_range):
+            neutral_value = x - (-x_cutoff - x_fuzzy_range) / (x_fuzzy_range * 2)
+            xvec = [1 - neutral_value, neutral_value, 0]
+            # normalize the vector
+            length = math.sqrt(numpy.dot(xvec, xvec))
+            xvec = numpy.divide(xvec, length)
+        elif (x_cutoff - x_fuzzy_range < x < x_cutoff + x_fuzzy_range):
+            positive_value = x - (x_cutoff - x_fuzzy_range) / (x_fuzzy_range * 2)
+            xvec = [0, 1 - positive_value, positive_value]
+            # normalize the vector
+            length = math.sqrt(numpy.dot(xvec, xvec))
+            xvec = numpy.divide(xvec, length)
+
+        y_cutoff = x_cutoff * y_scalar
+        y_fuzzy_range = x_fuzzy_range * y_scalar
+        if (y >= y_cutoff + y_fuzzy_range):
+            yvec = [0, 0, 1]
+        elif (y <= -y_cutoff - y_fuzzy_range):
+            yvec = [1, 0, 0]
+        elif (-y_cutoff - y_fuzzy_range <= y <= y_cutoff - y_fuzzy_range):
+            yvec = [0, 1, 0]
+        elif (-y_cutoff - y_fuzzy_range > y > -y_cutoff + y_fuzzy_range):
+            neutral_value = y - (-y_cutoff - y_fuzzy_range) / (y_fuzzy_range * 2)
+            yvec = [1 - neutral_value, neutral_value, 0]
+            # normalize the vector
+            length = math.sqrt(numpy.dot(yvec, yvec))
+            yvec = numpy.divide(yvec, length)
+        elif (y_cutoff - y_fuzzy_range < y < y_cutoff + y_fuzzy_range):
+            positive_value = y - (y_cutoff - y_fuzzy_range) / (y_fuzzy_range * 2)
+            yvec = [0, 1 - positive_value, positive_value]
+            # normalize the vector
+            length = math.sqrt(numpy.dot(yvec, yvec))
+            yvec = numpy.divide(yvec, length)
+
+        correct += numpy.dot(xvec, yvec)
+
+    return correct / float(num_points)
+
+
 def mae(x_values, y_values):
     '''Mean absolute/unsigned error.'''
     import numpy
@@ -43,8 +115,7 @@ def mae(x_values, y_values):
     return numpy.sum(numpy.apply_along_axis(numpy.abs, 0, numpy.subtract(x_values, y_values))) / float(num_points)
 
 
-def get_xy_dataset_correlations(x_values, y_values, fcorrect_x_cutoff = 1.0, fcorrect_y_cutoff = 1.0):
-    from scipy.stats import pearsonr, spearmanr, normaltest, ks_2samp, kstest, norm
+def get_xy_dataset_correlations(x_values, y_values, fcorrect_x_cutoff = 1.0, fcorrect_y_cutoff = 1.0, x_fuzzy_range = 0.1, y_scalar = 1.0):
     assert(len(x_values) == len(y_values))
     return dict(
         pearsonr = pearsonr(x_values, y_values),
@@ -57,6 +128,7 @@ def get_xy_dataset_correlations(x_values, y_values, fcorrect_x_cutoff = 1.0, fco
         kstesty = kstest(y_values, 'norm'),
         ks_2samp = ks_2samp(x_values, y_values),
         fraction_correct = fraction_correct(x_values, y_values, x_cutoff = fcorrect_x_cutoff, y_cutoff = fcorrect_y_cutoff),
+        fraction_correct_fuzzy_linear = fraction_correct_fuzzy_linear(x_values, y_values, x_cutoff = fcorrect_x_cutoff, x_fuzzy_range = x_fuzzy_range, y_scalar = y_scalar),
     )
 
 
