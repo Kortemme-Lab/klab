510515b630e4342a61b97dd744b300a683ec23ba
Shane O'Connor <spiderbaby@gmail.com>
2016-01-08 12:26:00 -0800

    Adding a HTTPConnection wrapper class which plays nicer. The previous functions opened a new connection for each request. When requesting many resources from a site, this behavior can trigger automatic connection refusal.

--- klab/comms/http.py
+++ klab/comms/http.py
@@ -10,7 +10,8 @@ Created by Shane O'Connor 2012
 import os
 from httplib import HTTPConnection
 
-def get(url):
+
+def get(url, timeout = None):
     url = url.strip()
     if url[:7].lower()==("http://"):
         url = url[7:]
@@ -18,15 +19,16 @@ def get(url):
 
     root = url[:idx]
     resource = url[idx:]
-    c = HTTPConnection(root)
+    c = HTTPConnection(root, timeout = timeout)
     c.request("GET", resource)
     response = c.getresponse()
     contents = response.read()
     c.close()
     return contents
 
-def get_resource(url, resource):
-    c = HTTPConnection(url)
+
+def get_resource(url, resource, timeout = None):
+    c = HTTPConnection(url, timeout = timeout)
     c.request("GET", resource)
     response = c.getresponse()
     contents = response.read()
@@ -34,3 +36,58 @@ def get_resource(url, resource):
     if contents[0:6] == "<html>":
         raise Exception("Error retrieving %s." % os.path.split(url)[1])
     return contents
+
+
+
+class Connection(object):
+    '''A class to keep a HTTPConnection open for multiple requests.'''
+
+    def __init__(self, url, timeout = None, attempts = 3):
+        self.timeout = timeout
+        self.attempts = attempts
+        self.url = url
+        self.connection = None
+
+
+    def _get_connection(self):
+        if self.connection:
+            return self.connection
+        else:
+            if self.timeout:
+                self.connection = HTTPConnection(self.url, timeout = self.timeout)
+            else:
+                self.connection = HTTPConnection(self.url)
+
+
+    def __del__(self):
+        if self.connection:
+            self.connection.close()
+
+
+    def _close(self):
+        if self.connection: self.connection.close()
+
+
+    def get(self, resource):
+        self._get_connection()
+        attempts_left = self.attempts
+        while attempts_left > 0:
+            try:
+                self.connection.request("GET", resource)
+                response = self.connection.getresponse()
+                contents = response.read()
+                if contents[0:6] == "<html>":
+                    raise Exception("Error retrieving %s." % os.path.split(self.url)[1])
+                return contents
+            except Exception, e:
+                import traceback
+                print(str(e))
+                print(traceback.format_exc())
+                self._close()
+        raise Exception('get {0} failed'.format(resource))
+
+
+    @classmethod
+    def get_resource(cls, url, resource):
+        c = cls(url)
+        return c.get(resource)
