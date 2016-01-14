fe2e795c98bd1c9d3487e20971f9c8abd6dcb015
Kyle Barlow <kb@kylebarlow.com>
2016-01-13 16:14:15 -0800

    Improved time estimate reporting (with hours, days, weeks)

--- klab/Reporter.py
+++ klab/Reporter.py
@@ -15,6 +15,7 @@ class Reporter:
         if self.print_output:
             print '\nStarting ' + task
         self.total_count = None # Total tasks to be processed
+        self.maximum_output_string_length = 0
     def set_total_count(self, x):
         self.total_count = x
     def decrement_total_count(self):
@@ -29,11 +30,48 @@ class Reporter:
                 time_now = time.time()
                 est_total_time = (time_now - self.start) * (1.0 / percent_done)
                 time_remaining = est_total_time - (time_now - self.start)
+                time_remaining_str = 'Est. time remaining: '
+
+                weeks_remaining = math.floor(time_remaining / 604800.0)
+                if weeks_remaining > 0:
+                    time_remaining_str += '%d weeks' % weeks_remaining
+                time_remaining -= weeks_remaining * 604800.0
+
+                days_remaining = math.floor(time_remaining / 86400.0)
+                if days_remaining > 0:
+                    if time_remaining_str[-1] != ' ' and time_remaining_str[-1] != ',':
+                        time_remaining_str += ', '
+                    time_remaining_str += '%d days' % days_remaining
+                time_remaining -= days_remaining * 86400.0
+
+                hours_remaining = math.floor(time_remaining / 3600.0)
+                if hours_remaining > 0:
+                    if time_remaining_str[-1] != ' ' and time_remaining_str[-1] != ',':
+                        time_remaining_str += ', '
+                    time_remaining_str += '%d hours' % hours_remaining
+                time_remaining -= hours_remaining * 3600.0
+
                 minutes_remaining = math.floor(time_remaining / 60.0)
-                seconds_remaining = int(time_remaining - (60*minutes_remaining))
-                sys.stdout.write("  Processed: %d %s (%.1f%%) %02d:%02d\r" % (n, self.entries, percent_done*100.0, minutes_remaining, seconds_remaining) )
+                if minutes_remaining > 0:
+                    if time_remaining_str[-1] != ' ' and time_remaining_str[-1] != ',':
+                        time_remaining_str += ', '
+                    time_remaining_str += '%d minutes' % minutes_remaining
+                time_remaining -= minutes_remaining * 60.0
+                
+                seconds_remaining = int(time_remaining)
+                if time_remaining_str[-1] != ' ' and time_remaining_str[-1] != ',':
+                    time_remaining_str += ', '
+                time_remaining_str += '%d seconds' % seconds_remaining
+
+                output_string = "  Processed: %d %s (%.1f%%) %s\r" % (n, self.entries, percent_done*100.0, time_remaining_str)
             else:
-                sys.stdout.write("  Processed: %d %s\r" % (n, self.entries) )
+                output_string = "  Processed: %d %s\r" % (n, self.entries)
+
+            if len(output_string) > self.maximum_output_string_length:
+                self.maximum_output_string_length = len(output_string)
+            elif len(output_string) < self.maximum_output_string_length:
+                output_string = output_string.ljust(self.maximum_output_string_length)
+            sys.stdout.write( output_string )
             sys.stdout.flush()
 
     def increment_report(self):
