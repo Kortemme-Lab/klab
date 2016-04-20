891825ecf08ddbcb9e649274094638406673b2de
Shane O'Connor <spiderbaby@gmail.com>
2016-04-19 18:48:06 -0700

    Adding a class for caching PDB file contents and klab.bio.pdb::PDB objects. The motivation for this was in noticing how badly I handle file IO when using the relatrix and other code e.g. spiderwebs reads in the same PDB file three times and creates a relatrix instance which then reads in the same PDB file another three times. In some of these cases, we construct a PDB object which is not cheap. My solution to this inefficiency is to pass around a cache object which keeps the file contents and PDB objects in memory and accesses them on demand. The user can set limits on the cache size and on whether files are saved to disk.
    The cache currently orders objects by their insertion time. In the next update, I will change this to order objects by their access time.
    
    A test script is also included.

---
+++ .test/bio/cache.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import time
+import pprint
+
+sys.path.insert(0, os.path.join('..', '..'))
+
+from klab import colortext
+from klab.bio.cache import PDBCache
+
+
+# Simple test
+colortext.message('\nSimple test')
+c = PDBCache(max_capacity = 10)
+for x in xrange(100):
+    c.add_pdb_contents('%04d' % x, str(x * x - 1))
+pprint.pprint(c.pdb_contents)
+time.sleep(0.010)
+
+# PDB test
+colortext.message('\nPDB test')
+c = PDBCache(cache_dir = '/tmp', max_capacity = None)
+for pdb_id in ['1A22', '1A4Y', '1ACB', '1AHW', '1AK4', '1BRS', '1CBW']:
+    c.get_pdb_contents(pdb_id)
+pprint.pprint(c.pdb_contents) # there should be 7 PDBs
+assert(sorted(c.pdb_contents.keys()) == sorted(['1A22', '1A4Y', '1ACB', '1AHW', '1AK4', '1BRS', '1CBW']))
+
+c.max_capacity = 4
+c.get_pdb_contents('1CHO') # this should get the new PDB and truncate the list to 4 elements
+pprint.pprint(c.pdb_contents) # there should be 4 PDBs
+assert(sorted(c.pdb_contents.keys()) == sorted(['1AK4', '1BRS', '1CBW', '1CHO']))
+
+# Re-read in an old file which was thrown away above - this should trigger another file read and erase an older entry
+c.get_pdb_contents('1A22')
+pprint.pprint(c.pdb_contents) # there should be 4 PDBs
+assert(sorted(c.pdb_contents.keys()) == sorted(['1BRS', '1CBW', '1CHO', '1A22']))
