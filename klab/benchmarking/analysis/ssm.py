295fb4ad701e6f2ec18e6094d669b7231a0c355a
Shane O'Connor <spiderbaby@gmail.com>
2016-07-15 20:34:03 -0700

    Wrapping Samuel's standard-deviation XY analysis in utility functions to interface with other modules.

--- klab/benchmarking/analysis/ssm.py
+++ klab/benchmarking/analysis/ssm.py
@@ -37,6 +37,29 @@ from klab import colortext
 from klab.fs.fsio import get_file_lines
 
 
+# Used to rename fields to match the conventions used in misc.py
+field_name_mapper = (
+    ('R_value', 'pearsonr'),
+    ('slope', 'pearsonr_slope'),
+    ('R_value_through_origin', 'pearsonr_origin'),
+    ('slope_through_origin', 'pearsonr_slope_origin'),
+    ('STDev_cutoff', 'std_dev_cutoff'),
+    ('warnings', 'warnings'),
+    ('MAE', 'MAE'),
+    ('scaled_MAE', 'scaled_MAE'),
+    ('accuracy', 'accuracy'),
+    ('specificity', 'specificity'),
+    ('sensitivity', 'sensitivity'),
+    ('significance_specificity', 'significance_specificity'),
+    ('significance_sensitivity', 'significance_sensitivity'),
+)
+
+
+####################
+# Utility functions
+####################
+
+
 def least_squares_fixed_origin(x, y):
     x_linalg = x[:, np.newaxis]
     linalg_slope, linalg_residual = np.linalg.lstsq(x_linalg, y)[:2]
@@ -196,27 +219,35 @@ def calculate_mae(prediction_data, experimental_data):
     warning = ""
 
     mae = np.mean(np.abs(np.array(prediction_data['array']) - np.array(experimental_data['array'])))
-    scaled_prediction = prediction_data['array'] * np.abs(prediction_data['slope through origin'])
+    scaled_prediction = prediction_data['array'] * np.abs(prediction_data['slope_through_origin'])
     scaled_mae = np.mean(np.abs(np.array(scaled_prediction) - np.array(experimental_data['array'])))
 
-    if prediction_data['slope through origin'] < 0.0:
+    if prediction_data['slope_through_origin'] < 0.0:
         print "Warning: Data is anti-correlated for regression through the origin.\nMean absolute error is not an effective metric for anti-correlated data."
         warning = "MAE is not applicable for anti-correlated data."
     return mae, scaled_mae, warning
 
 
-def parse_csv(csv_filepath, expect_negative_correlation = False, STDev_cutoff = 1.0, headers_start_with = 'ID'):
+####################
+# Main functionality
+####################
+
+
+def parse_csv(csv_lines, expect_negative_correlation = False, STDev_cutoff = 1.0, headers_start_with = 'ID', comments_start_with = None, separator = ','):
     """
-    Expects a CSV file with a header line starting with headers_start_with e.g. "ID,experimental value, prediction 1 value, prediction 2 value,"
+    Analyzes lines in CSV format.
+
+    Expects the first line to be a header line starting with headers_start_with e.g. "ID,experimental value, prediction 1 value, prediction 2 value,"
     Record IDs are expected in the first column.
     Experimental values are expected in the second column.
     Predicted values are expected in the subsequent columns.
 
-    :param csv_filepath: The path to a CSV file containing experimental and predicted data for some dataset.
+    :param csv_lines: Lines read from a CSV file containing experimental and predicted data for some dataset.
     :param expect_negative_correlation: Indicates wheter or not the predictions have the same or opposite sign as the experimental data.
     :param STDev_cutoff: Number of standard deviations as a threshold for significance cut-off. Used for both predictions and experiments.
     :param headers_start_with: A string used to distinguish the header line.
-
+    :param comments_start_with: A string used to distinguish lines containing comments to be ignored.
+    :param separator: Column separator. For CSV files, ',' should be used. For TSV files, '\t' should be used.
     """
     data = {}
     data['experimental'] = {}
@@ -224,15 +255,24 @@ def parse_csv(csv_filepath, expect_negative_correlation = False, STDev_cutoff = 
     data['experimental']['dict'] = {}
     data['predictions'] = {}
 
-    assert(os.path.exists(csv_filepath))
-    lines = get_file_lines(csv_filepath)
+    if comments_start_with != None:
+        lines = [l for l in csv_lines if l.strip() and not(l.strip().startswith(comments_start_with))]
+    else:
+        lines = [l for l in csv_lines if l.strip()]
+
+    print(lines[0], '&')
+
+    headers = lines[0]
+    assert(headers.startswith(headers_start_with))
+    headers = headers.split(separator)
+    num_fields = len(headers)
+    prediction_names = headers[2:]
 
     skipped_cases = []
-    for line in lines:
-        if line[:2] == "ID":
-            continue  # Skip the header if it is present
+    for line in lines[1:]:
 
-        fields = line.split(',')
+        fields = [t.strip() for t in line.split(separator)]
+        assert(len(fields) == num_fields)
 
         mut_name = fields[0]
         expt_value = fields[1]
@@ -249,73 +289,160 @@ def parse_csv(csv_filepath, expect_negative_correlation = False, STDev_cutoff = 
         counter = 0
         for value in predictions:
             counter += 1
-            prediction_name = str(counter)
+            prediction_id = counter
+            prediction_name = prediction_names[counter - 1]
 
-            try:
-                test = data['predictions'][prediction_name]
-            except KeyError:
-                data['predictions'][prediction_name] = {}
+            data['predictions'][prediction_id] = data['predictions'].get(prediction_id, dict(name = prediction_name, id = prediction_id))
+            prediction_cases = data['predictions'][prediction_id]
 
-            try:
-                test = data['predictions'][prediction_name]['list']
-            except KeyError:
-                data['predictions'][prediction_name]['list'] = []
-                data['predictions'][prediction_name]['dict'] = {}
+            prediction_cases['list'] = prediction_cases.get('list', [])
+            prediction_cases['dict'] = prediction_cases.get('dict', {})
 
-            data['predictions'][prediction_name]['dict']['mut_name'] = float(value)
-            data['predictions'][prediction_name]['list'].append(float(value))
+            prediction_cases['dict']['mut_name'] = float(value)
+            prediction_cases['list'].append(float(value))
 
-    for prediction_name in data['predictions'].keys():
-        data['predictions'][prediction_name]['array'] = np.array(data['predictions'][prediction_name]['list'])
-        pos_STDev, neg_STDev = get_std_dev(data['predictions'][prediction_name]['array'])
-        data['predictions'][prediction_name]['positive significance threshold'] = pos_STDev
-        data['predictions'][prediction_name]['negative significance threshold'] = neg_STDev
+    data['skipped_cases'] = skipped_cases
+    for prediction_id, prediction_cases in data['predictions'].iteritems():
+        prediction_cases['array'] = np.array(prediction_cases['list'])
+        pos_STDev, neg_STDev = get_std_dev(prediction_cases['array'])
+        prediction_cases['positive significance threshold'] = pos_STDev
+        prediction_cases['negative significance threshold'] = neg_STDev
 
     data['experimental']['array'] = np.array(data['experimental']['list'])
     pos_STDev, neg_STDev = get_std_dev(data['experimental']['array'])
     data['experimental']['positive significance threshold'] = pos_STDev
     data['experimental']['negative significance threshold'] = neg_STDev
 
-    for prediction_name in data['predictions'].keys():
-        slope_through_origin, r_value_through_origin = least_squares_fixed_origin(
-                data['predictions'][prediction_name]['array'], data['experimental']['array'])
-        slope, intercept, r_value, p_value, std_err = stats.linregress(data['predictions'][prediction_name]['array'],
-                                                                       data['experimental']['array'])
-        data['predictions'][prediction_name]['warnings'] = []
-        data['predictions'][prediction_name]['slope through origin'] = slope_through_origin
-        data['predictions'][prediction_name]['R-value through origin'] = r_value_through_origin
-        data['predictions'][prediction_name]['slope'] = slope
-        data['predictions'][prediction_name]['R value'] = r_value
-        mae, scaled_mae, warning = calculate_mae(data['predictions'][prediction_name], data['experimental'])
-        data['predictions'][prediction_name]['MAE'] = mae
-        data['predictions'][prediction_name]['scaled MAE'] = scaled_mae
-        data['predictions'][prediction_name]['warnings'].append(warning)
-        accuracy, specificity, sensitivity, significance_specificity, significance_sensitivity = determine_correct_sign(
-                data['predictions'][prediction_name], data['experimental'], expect_negative_correlation, STDev_cutoff)
-        data['predictions'][prediction_name]['accuracy'] = accuracy
-        data['predictions'][prediction_name]['specificity'] = specificity
-        data['predictions'][prediction_name]['sensitivity'] = sensitivity
-        data['predictions'][prediction_name]['significance specificity'] = significance_specificity
-        data['predictions'][prediction_name]['significance sensitivity'] = significance_sensitivity
+    for prediction_id, prediction_cases in sorted(data['predictions'].iteritems()):
+        slope_through_origin, r_value_through_origin = least_squares_fixed_origin(prediction_cases['array'], data['experimental']['array'])
+        slope, intercept, r_value, p_value, std_err = stats.linregress(prediction_cases['array'], data['experimental']['array'])
 
-        if skipped_cases:
-            colortext.warning('\nSkipped {0} cases due to partial data: {1}.\n'.format(len(skipped_cases), ', '.join(skipped_cases)))
-        print str("Prediction number: %s") % prediction_name
-        print str("\tR-value: %03f\t\tSlope: %03f") % (r_value, slope)
-        print str("\tR-value with origin as intercept: %03f\t\tSlope: %03f") % (r_value_through_origin, slope_through_origin)
-        print str("\tMAE: %03f\t\t Scaled MAE: %03f") % (mae, scaled_mae)
+        prediction_cases.update(dict(
+            prediction_id = prediction_id,
+            slope_through_origin = slope_through_origin,
+            R_value_through_origin = r_value_through_origin,
+            slope = slope,
+            R_value = r_value,
+        ))
 
-        print str("\n\tClassifications:\n\t\t-- Cut-off for significance at %f STDevs -- ") % STDev_cutoff
-        print str("\tPercent correct sign (accuracy): %03f") % accuracy
-        print str("\tPercent significant predictions with correct sign (specificity): %03f") % specificity
-        print str("\tPercent significant experimental hits with correct prediction sign (sensitivity): %03f") % sensitivity
-        print str("\tPercent significant predictions are significant experimental hits (significance specificity): %03f") % significance_specificity
-        print str("\tPercent significant experimental hits are predicted significant hits (significance sensitivity): %03f") % significance_sensitivity
-        print str("\n\n")
+        warnings = []
+        mae, scaled_mae, warning = calculate_mae(prediction_cases, data['experimental'])
+        accuracy, specificity, sensitivity, significance_specificity, significance_sensitivity = determine_correct_sign(prediction_cases, data['experimental'], expect_negative_correlation, STDev_cutoff)
+        if warning:
+            warnings = [warning]
+
+        prediction_cases.update(dict(
+            STDev_cutoff = STDev_cutoff,
+            warnings = warnings,
+            MAE = mae,
+            scaled_MAE = scaled_mae,
+            accuracy = accuracy,
+            specificity = specificity,
+            sensitivity = sensitivity,
+            significance_specificity = significance_specificity,
+            significance_sensitivity = significance_sensitivity,
+        ))
+    return data
+
+
+#######################
+# Derived functionality
+#######################
+
+
+def parse_csv_file(csv_filepath, expect_negative_correlation = False, STDev_cutoff = 1.0, headers_start_with = 'ID', comments_start_with = None, separator = ','):
+    """
+    Analyzes a CSV file.
+    Expects a CSV file with a header line starting with headers_start_with e.g. "ID,experimental value, prediction 1 value, prediction 2 value,"
+    Record IDs are expected in the first column.
+    Experimental values are expected in the second column.
+    Predicted values are expected in the subsequent columns.
+
+    :param csv_filepath: The path to a CSV file containing experimental and predicted data for some dataset.
+    :param expect_negative_correlation: See parse_csv.
+    :param STDev_cutoff: See parse_csv.
+    :param headers_start_with: See parse_csv.
+    :param comments_start_with: See parse_csv.
+    :param separator: See parse_csv.
+    """
+    assert (os.path.exists(csv_filepath))
+    return parse_csv(get_file_lines(csv_filepath),
+                     expect_negative_correlation = expect_negative_correlation, STDev_cutoff = STDev_cutoff, headers_start_with = headers_start_with,
+                     comments_start_with = comments_start_with, separator = separator)
+
+
+def get_std_xy_dataset_statistics(x_values, y_values, expect_negative_correlation = False, STDev_cutoff = 1.0):
+    '''Calls parse_csv and returns the analysis in a format similar to get_xy_dataset_statistics in klab.stats.misc.'''
+    assert(len(x_values) == len(y_values))
+    csv_lines = ['ID,X,Y'] + [','.join(map(str, [c + 1, x_values[c], y_values[c]])) for c in xrange(len(x_values))]
+    data = parse_csv(csv_lines, expect_negative_correlation = expect_negative_correlation, STDev_cutoff = STDev_cutoff)
+
+    assert(len(data['predictions']) == 1)
+    assert(1 in data['predictions'])
+    assert(data['predictions'][1]['name'] == 'Y')
+    summary_data = data['predictions'][1]
+
+    stats = {}
+    for spair in field_name_mapper:
+        stats[spair[1]] = summary_data[spair[0]]
+    if stats['warnings']:
+        stats['warnings'] = '\n'.join(stats['warnings'])
+    else:
+        stats['warnings'] = None
+    return stats
+
+
+def analyze_dataframe(df, id_field_column, reference_field_column, prediction_field_columns = [],
+                          expect_negative_correlation = False, STDev_cutoff = 1.0):
+    """Takes a dataframe, converts it to a CSV, and runs parse_csv.
+       Typically, we would work the other way around, converting a CSV to dataframe. However, this was quicker to implement with the existing code.
+
+       :param df: A pandas dataframe.
+       :param id_field_column: The column index (zero-indexed) whose corresponding column contains the ID (record/case ID) field. Values in this field must be integer values.
+       :param reference_field_column: The column index (zero-indexed) whose corresponding column contains the reference data (X-axis) e.g. experimental measurements or reference data. Values in this field are assumed to be integers or floating-point values. Otherwise, they will be imported as NaN.
+       :param prediction_field_columns: The column indices (zero-indexed) whose corresponding columns contains the prediction data (Y-axis). If this parameter is unspecified, all columns excepting the ID and reference columns are treated as predictions.
+       :param expect_negative_correlation: See parse_csv.
+       :param STDev_cutoff: See parse_csv.
+       :return:
+    """
+    raise Exception('Implement this using parse_csv.')
+
+
+####################
+# Reporters
+####################
+
+
+def print_summary(data):
+
+    if data['skipped_cases']:
+        colortext.warning('\nSkipped {0} cases due to partial data: {1}.\n'.format(len(data['skipped_cases']),
+                                                                                   ', '.join(data['skipped_cases'])))
+    for prediction_id, prediction_cases in sorted(data['predictions'].iteritems()):
+
+        print('''
+Prediction: #{id}
+Prediction name: {name}
+\tR-value: {R_value:.03f}\t\tSlope: {slope:.03f}
+\tR-value with origin as intercept: {R_value_through_origin:.03f}\t\tSlope: {slope_through_origin:.03f}
+\tMAE: {MAE:.03f}\t\t Scaled MAE: {scaled_MAE:.03f}
+
+\n\tClassifications:\n\t\t-- Cut-off for significance at {STDev_cutoff:f} STDevs --
+\tPercent correct sign (accuracy): {accuracy:.03f}
+\tPercent significant predictions with correct sign (specificity): {specificity:.03f}
+\tPercent significant experimental hits with correct prediction sign (sensitivity): {sensitivity:.03f}
+\tPercent significant predictions are significant experimental hits (significance specificity): {significance_specificity:.03f}
+\tPercent significant experimental hits are predicted significant hits (significance sensitivity): {significance_sensitivity:.03f}
+\n\n'''.format(**prediction_cases))
+
+
+######################
+# Command-line handler
+######################
 
 
 if __name__ == '__main__':
     datafilename = sys.argv[1]
     print str("\n\n--Analysis of predictions in %s--") % datafilename
-    parse_csv(datafilename, expect_negative_correlation, STDev_cutoff = 1.0, headers_start_with = 'ID')
+    parse_csv_file(datafilename, expect_negative_correlation, STDev_cutoff = 1.0, headers_start_with = 'ID')
 
