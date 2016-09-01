53462df042707c3d106a35b0fc420dcbdb354d42
Shane O'Connor <spiderbaby@gmail.com>
2016-09-01 16:48:57 -0700

    Adding codon translation table.

--- klab/bio/basics.py
+++ klab/bio/basics.py
@@ -243,6 +243,33 @@ all_recognized_rna = rna_nucleotides.union(set(non_canonical_rna.keys()))
 
 nucleotide_types_1 = set(dna_nucleotides_2to1_map.values()) # for use in SEQRES sequences
 
+gencode = {
+    # Source - http://adamcoster.com/2011/01/13/python-clean-up-and-translate-nucleotide-sequences/
+    'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
+    'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
+    'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
+    'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
+    'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
+    'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
+    'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
+    'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
+    'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
+    'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
+    'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
+    'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
+    'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
+    'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
+    'TAC': 'Y', 'TAT': 'Y', 'TAA': '.', 'TAG': '.',
+    'TGC': 'C', 'TGT': 'C', 'TGA': '.', 'TGG': 'W'
+}
+
+
+def translate_codons(sequence):
+    '''Return the translated protein from 'sequence' assuming +1 reading frame
+       Source - http://adamcoster.com/2011/01/13/python-clean-up-and-translate-nucleotide-sequences/
+    '''
+    return ''.join([gencode.get(sequence[3*i:3*i+3],'X') for i in range(len(sequence)//3)])
+
 
 # Atoms
 
